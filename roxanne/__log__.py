b9b89ab4d9cd5af9c26528b2d9a85655645d56a9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-06 19:20:21 -0300

    Created roxanne/__log__.py at Timestamp: 2019-08-06 22:20:20

---
+++ roxanne/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Aug 06 2019 19:20:19.25 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 18
+  class doidera()
+                  ^
+SyntaxError: invalid syntax
+'''},
