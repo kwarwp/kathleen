6571c228e9428f9d05a8e43724c7b7e84b8cc0b7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-10 14:03:59 -0300

    Saved libby/main.py at Timestamp: 2019-08-10 17:03:58

--- libby/main.py
+++ libby/main.py
@@ -8,3 +8,21 @@ LABORATORIO= "
 HOSPITAL= "
 BIBLIOTECA= "
 LAB_INFORMATICA= "
+SALA1= "img norte LABORATORIO"
+SALA2= "img leste HOSPITAL"
+SALA3= "img oeste BIBLIOTECA"
+SALA4= "img sul LAB_INFORMATICA"
+Class HAHA():
+init_main (self)
+ana_maria= Elemento(img ANA_MARIA)
+dr_zuckman= Elemento(img DR_ZUCKMAN)
+cofre= Elemento(img COFRE)
+laboratorio= Cena(img LABORATORIO)
+hospital= Cena(img HOSPITAL)
+biblioteca= Cena(img BIBLIOTECA)
+lab_informatica= Cena(img LAB_INFORMATICA)
+hehe= LABIRINTO (norte= SALA1,leste= SALA2,oeste= SALA3,sul= SALA4)
+ana_maria.entra(hehe.norte)
+dr_zuckman.entra(hehe.norte)
+hehe.norte.vai()
+HAHA()
