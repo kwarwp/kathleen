a588f476aa77902c69e133fc8b29f9c31dd43d0d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-10 13:17:20 -0300

    Created libby/main.py at Timestamp: 2019-08-10 16:17:19

---
+++ libby/main.py
@@ -0,0 +1,10 @@
+# kathleen.libby.main.py
+from _spy.vittolino.main import Cena,Elemento,Labirinto
+from _spy.vittolino.main import INVENTARIO as inv
+ANA_MARIA= "
+DR_ZUCKMAN= "
+COFRE= "
+LABORATORIO= "
+HOSPITAL= "
+BIBLIOTECA= "
+LAB_INFORMATICA= "
