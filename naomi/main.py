4816f71c3af548981b3259cb852c645ee26959a2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-20 18:06:35 -0300

    Created naomi/main.py at Timestamp: 2019-08-20 21:06:35

---
+++ naomi/main.py
@@ -0,0 +1,21 @@
+# kathleen.naomi.main.py
+from_spy.vittolino.main import INVENTÁRIO as inv 
+import Cena, Elemento, Labirinto 
+from_spy.vittolino.main import
+Ana_Maria= “imagem em jpe ou png ...**”
+Dr_Zuckman= “imagem em jpe ou png”
+Sala 1 = “imagem do laboratório”
+SALA 2 =“imagem do hospital”
+SALA 3 = “imagem biblioteca”
+SALA 4= “imag laboratório de computação”
+
+Class Jesus ();
+def _init_main (self)
+ana_maria= Elemento (img= Ana_Maria)
+dr_zuckman= Elemento (img= Dr_Zuckman)
+lugar = Labirinto (n= SALA 1, l=SALA 2, o= SALA 3, s= SALA 4)
+ana_maria.entra(lugar.norte)
+dr_zuckman.entra( lugar.norte)
+lugar.norte.vai()
+Jesus ()
+
