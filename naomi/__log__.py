3fed9f93d93b3a950a849e6346e8d8b914495062
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-20 17:56:45 -0300

    Saved naomi/__log__.py at Timestamp: 2019-08-20 20:56:45

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -3,3 +3,10 @@
 'error': '''
 Exception: <SyntaxError: Unexpected token {>
 '''},
+{'date': 'Tue Aug 20 2019 17:56:45.623 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  from_spy.vittolino.main import
+                                 ^
+SyntaxError: invalid syntax
+'''},
