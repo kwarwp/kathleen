076c71a8509fbb3cf8b3e60a9cb29c200bb92ed2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-21 17:16:07 -0300

    Created sara/__log__.py at Timestamp: 2019-08-21 20:16:06

---
+++ sara/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Aug 21 2019 17:16:07.366 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 13
+  def _init_(self)
+                   ^
+SyntaxError: invalid syntax
+'''},
