a7c8c4c501bb8650ae37ad4eaadeb25e6262a2f5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-21 17:17:29 -0300

    Saved sara/main.py at Timestamp: 2019-08-21 20:17:29

--- sara/main.py
+++ sara/main.py
@@ -10,7 +10,7 @@ SALA1="http://lorempixel.com/800/600/city"
 SALA2="http://lorempixel.com/800/600/city"
 
 class CHOCOLATE():
-    def __init__(self)
+    def __init__(self):
     ana_maria= Elemento(img=ANA_MARIA)
     dr_duckman= Elemento(img=DR_DUCKMAN)
     laboratori= Cena(img=LABORATORIO)
