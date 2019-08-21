a5009f8efb0981b5f8a9475b0930f6d8c5ec47d2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-21 16:36:18 -0300

    Created libby/__log__.py at Timestamp: 2019-08-21 19:36:18

---
+++ libby/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Aug 21 2019 16:36:19.328 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  ANA_MARIA= "
+              ^
+SyntaxError: EOL while scanning string literal
+'''},
