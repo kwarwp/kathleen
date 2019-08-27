a452e2b6e26bbf4e8c5dbd8987422dc4b6b59b66
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-27 17:51:23 -0300

    Created stacy/__log__.py at Timestamp: 2019-08-27 20:51:23

---
+++ stacy/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Aug 27 2019 17:51:21.323 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 19
+  def __init__ (self)
+                      ^
+SyntaxError: invalid syntax
+'''},
