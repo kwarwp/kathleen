d470228f6c80a8b4c4aec3923c0c29c49aa67a26
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-28 11:13:12 -0300

    Created grace/__log__.py at Timestamp: 2019-08-28 14:13:12

---
+++ grace/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Aug 28 2019 11:13:11.320 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  ESPACO = https://img2.gratispng.com/20180630/lyt/kisspng-atmosphere-desktop-wallpaper-space-computer-univer-consciousness-5b374ccd919912.1150048715303507975964.jpg
+                 ^
+SyntaxError: invalid syntax
+'''},
