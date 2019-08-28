ba11b8e43999fb9c99f88355ac0f8bfd9b1472ad
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-28 11:11:09 -0300

    Saved courtney/__log__.py at Timestamp: 2019-08-28 14:11:09

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -6,3 +6,10 @@
        ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 28 2019 11:11:28.804 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  JURUPINGA= "https://hostelbraz.com.br/images/praias/grumari/hostelBraz_praia-grumari_03.jpg
+                                                                                             ^
+SyntaxError: EOL while scanning string literal
+'''},
