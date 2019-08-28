8f4b18763605bae573e11af4ffa79d9a37fef243
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-28 11:15:24 -0300

    Created julia/__log__.py at Timestamp: 2019-08-28 14:15:23

---
+++ julia/__log__.py
@@ -0,0 +1,19 @@
+
+{'date': 'Wed Aug 28 2019 11:15:23.469 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 8
+    class fundo ():
+  module <module> line 9
+    def__int__(self)
+NameError: name 'def__int__' is not defined
+'''},
