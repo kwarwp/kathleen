d81dd7b3841021add030656d8f4f08a9f9ee9d47
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-28 11:09:24 -0300

    Created courtney/__log__.py at Timestamp: 2019-08-28 14:09:23

---
+++ courtney/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Aug 28 2019 11:09:40.890 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  from -spy.vittolino.main import Cena,Elemento,Labirinto 
+       ^
+SyntaxError: invalid syntax
+'''},
