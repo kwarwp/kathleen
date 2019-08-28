db27c4d3799878e92f2cdff81d6fe1fe2c2adcdf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-28 11:35:51 -0300

    Saved courtney/__log__.py at Timestamp: 2019-08-28 14:35:51

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -170,3 +170,21 @@ IndentationError: unexpected indent
                      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 28 2019 11:36:10.685 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 6
+    class masterkraken():
+  module <module> line 8
+    jurupinga=Cena(img=jurupinga)
+TypeError: 'module' object is not callable
+'''},
