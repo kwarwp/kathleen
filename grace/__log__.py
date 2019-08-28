fb3a16d4d26625dc13fd937bcc4dce0768536bc6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-28 11:17:02 -0300

    Saved grace/__log__.py at Timestamp: 2019-08-28 14:17:01

--- grace/__log__.py
+++ grace/__log__.py
@@ -31,3 +31,25 @@ SyntaxError: invalid syntax
     def__int__(self) 
 NameError: name 'def__int__' is not defined
 '''},
+{'date': 'Wed Aug 28 2019 11:17:03.251 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 5
+    class BFF():
+  module <module> line 7
+    espaco=Cena(img=ESPACO)
+TypeError: 'module' object is not callable
+'''},
