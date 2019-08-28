f1cad7db5d5c68e54a274d2d2a8a8f5c8d527588
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-28 11:08:55 -0300

    Saved morgan/__log__.py at Timestamp: 2019-08-28 14:08:55

--- morgan/__log__.py
+++ morgan/__log__.py
@@ -27,3 +27,10 @@ SyntaxError: invalid syntax
                  ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Aug 28 2019 11:08:56.692 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  def __ int __ (self):
+          ^
+SyntaxError: invalid syntax
+'''},
