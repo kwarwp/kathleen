9e1b68d7cac97ed89b13c41380ab3908f0e0c063
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-28 11:30:09 -0300

    Saved grace/__log__.py at Timestamp: 2019-08-28 14:30:06

--- grace/__log__.py
+++ grace/__log__.py
@@ -157,3 +157,21 @@ IndentationError: unexpected indent
     espaco=Cena(img=ESPACO)
 TypeError: 'module' object is not callable
 '''},
+{'date': 'Wed Aug 28 2019 11:30:07.839 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 5
+    class BFF():
+  module <module> line 7
+    espaco=Cena(img=ESPACO)
+TypeError: 'module' object is not callable
+'''},
