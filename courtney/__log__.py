af48e4c63b0531f1c8e9b4c222ba4a31a79f3acd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-28 11:15:36 -0300

    Saved courtney/__log__.py at Timestamp: 2019-08-28 14:15:36

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -55,3 +55,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Wed Aug 28 2019 11:15:55.902 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 10
+  cusco.entra(jurupinga)
+  ^
+IndentationError: unexpected indent
+'''},
