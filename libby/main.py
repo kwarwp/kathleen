5867f29c90b018d79d3f1a8feb874620c41f5e08
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-29 20:40:04 -0300

    Saved libby/main.py at Timestamp: 2019-08-29 23:40:04

--- libby/main.py
+++ libby/main.py
@@ -1,29 +1,29 @@
 # kathleen.libby.main.py
 from _spy.vittolino.main import Cena,Elemento,Labirinto
 from _spy.vittolino.main import INVENTARIO as inv
-ANA_MARIA= "
-DR_ZUCKMAN= "
-COFRE= "
-LABORATORIO= "
-HOSPITAL= "
-BIBLIOTECA= "
-LAB_INFORMATICA= "
+ANA_MARIA= "http://lorempixel.com/800/600/people/1"
+DR_ZUCKMAN= "http://lorempixel.com/800/600/people/2"
+COFRE= "http://lorempixel.com/800/600/city"
+LABORATORIO= "http://lorempixel.com/800/600/city"
+HOSPITAL= "http://lorempixel.com/800/600/city/2"
+BIBLIOTECA= "http://lorempixel.com/800/600/city/3"
+LAB_INFORMATICA= "http://lorempixel.com/800/600/city/4"
 SALA1= "img norte LABORATORIO"
 SALA2= "img leste HOSPITAL"
 SALA3= "img oeste BIBLIOTECA"
 SALA4= "img sul LAB_INFORMATICA"
 Class HAHA():
-init_main (self)
-ana_maria= Elemento(img ANA_MARIA)
-dr_zuckman= Elemento(img DR_ZUCKMAN)
-cofre= Elemento(img COFRE)
-laboratorio= Cena(img LABORATORIO)
-hospital= Cena(img HOSPITAL)
-biblioteca= Cena(img BIBLIOTECA)
-lab_informatica= Cena(img LAB_INFORMATICA)
-hehe= LABIRINTO (norte= SALA1,leste= SALA2,oeste= SALA3,sul= SALA4)
-ana_maria.entra(hehe.norte)
-dr_zuckman.entra(hehe.norte)
-hehe.norte.vai()
+  def __init__(self):
+ ana_maria= Elemento(img ANA_MARIA)
+ dr_zuckman= Elemento(img DR_ZUCKMAN)
+ cofre= Elemento(img COFRE)
+ laboratorio= Cena(img LABORATORIO)
+ hospital= Cena(img HOSPITAL)
+ biblioteca= Cena(img BIBLIOTECA)
+ lab_informatica= Cena(img LAB_INFORMATICA)
+ hehe= LABIRINTO (norte= SALA1,leste= SALA2,oeste= SALA3,sul= SALA4)
+ ana_maria.entra(hehe.norte)
+ dr_zuckman.entra(hehe.norte)
+ hehe.norte.vai()
 
 HAHA()
