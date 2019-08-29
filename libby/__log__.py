476b8e294e6d83db2d06c94f79f5cad8400377e2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-08-29 20:40:40 -0300

    Saved libby/__log__.py at Timestamp: 2019-08-29 23:40:40

--- libby/__log__.py
+++ libby/__log__.py
@@ -6,3 +6,10 @@
               ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Thu Aug 29 2019 20:40:42.909 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 15
+  Class HAHA():
+         ^
+SyntaxError: invalid syntax
+'''},
