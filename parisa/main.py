a99948ed849f984ee8ca653739ebad3013d23773
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 11:01:51 -0300

    Created parisa/main.py at Timestamp: 2019-09-04 14:01:51

---
+++ parisa/main.py
@@ -0,0 +1,6 @@
+# kathleen.parisa.main.py
+from_spy.vittolino.main import inventario as inv 
+import Cena, elemento labirinto 
+from_spy,vittolino.main import 
+Ana_Maria= "imagem em jpe ou png https://www.bing.com/images/search?view=detailV2&ccid=KQ7OAgXk&id=805CD751EAA953E96A1086FABDC96F804D0BD7C1&thid=OIP.KQ7OAgXkuXuVwqp5UcuQ9QHaFj&mediaurl=http%3a%2f%2fpngriver.com%2fwp-content%2fuploads%2f2018%2f04%2fDownload-Grass-PNG-Pic.png&exph=768&expw=1024&q=imagem+png&simid=608022116712449072&selectedIndex=50
+Dr_Zuckman= "imagem em jpe ou png
