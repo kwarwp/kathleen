9013a5d405ad18e69d387d6d75b1fa38cbd27d8a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 11:03:25 -0300

    Created samantha/main.py at Timestamp: 2019-09-04 14:03:25

---
+++ samantha/main.py
@@ -0,0 +1,8 @@
+# kathleen.samantha.main.py
+from _spy.vitollino.main import Cena, Elemento, Labirinto
+from _spy.vitollino.main import Inventario as inv
+
+from Sara.main import CHOCOLATE
+
+class BRIGADEIRO ():
+    
