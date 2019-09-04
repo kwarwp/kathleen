f793eacb45c65cd45bca3df5d655ecd1773cf8cb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 10:53:00 -0300

    Saved angie/main.py at Timestamp: 2019-09-04 13:53:00

--- angie/main.py
+++ angie/main.py
@@ -1,2 +1,11 @@
 # kathleen.angie.main.py
-from _spy.vittolino import Elemento , Cena, ,
+from _spy.vitollino.main import Cena, Elemento
+from _spy.vitollino.main import Inventario as inv
+from Naomi.main import Jesus
+PRAIA = "https://png.pngtree.com/element_origin_min_pic/16/05/28/095748f89ee93c0.jpg"
+class kk ():
+	praia = Cena(img = PRAIA)
+	praia.vai()
+kk()
+
+
