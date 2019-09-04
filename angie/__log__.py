a60191d0e49c2173dc2f6b2657ad01d4f82233a8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 10:49:42 -0300

    Created angie/__log__.py at Timestamp: 2019-09-04 13:49:42

---
+++ angie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Sep 04 2019 10:49:45.293 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  class kk
+           ^
+SyntaxError: invalid syntax
+'''},
