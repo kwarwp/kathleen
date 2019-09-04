f3b670efe00bf69164efac4fd48bb8115418c0dc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 10:36:48 -0300

    Saved libby/__log__.py at Timestamp: 2019-09-04 13:36:48

--- libby/__log__.py
+++ libby/__log__.py
@@ -90,3 +90,21 @@ SyntaxError: invalid syntax
                           ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Sep 04 2019 10:36:45.289 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 15
+    class HAHA():
+  module <module> line 18
+    ana_maria= Elemento(img=ANA_MARIA)
+TypeError: 'module' object is not callable
+'''},
