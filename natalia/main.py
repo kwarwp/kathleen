3f7d28252a9bf4b396c1a07acc4ebe4305247414
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 10:28:21 -0300

    Created natalia/main.py at Timestamp: 2019-09-04 13:28:21

---
+++ natalia/main.py
@@ -0,0 +1,8 @@
+# kathleen.natalia.main.py
+from _spy.vitollino.main import Cena, Elemento
+from _spy.vitollino.main import Inventario as inv
+CEN="https://4.bp.blogspot.com/-qOhCgMJvsPY/VxkW1ZBBrZI/AAAAAAAANwA/GNtWUUpfXg8rYfDBPF2SSadkF64CLRuMQCLcB/s1600/bg%2B04.png"
+class OLA ():
+	cen= Cena(img=CEN)
+	cen.vai()
+OLA()
