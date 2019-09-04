f10aca6e478e025dee742052b7681d87791f4f24
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 10:15:35 -0300

    Saved sara/__log__.py at Timestamp: 2019-09-04 13:15:35

--- sara/__log__.py
+++ sara/__log__.py
@@ -96,3 +96,33 @@ AttributeError: 'str' object has no attribute 'cenas'
                     ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Sep 04 2019 10:15:35.857 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 12
+    class CHOCOLATE():
+  module <module> line 18
+    papel= Labirinto(n=SALA1,l=SALA2)
+  module _spy.vitollino.main line 646
+    self.lb()
+  module _spy.vitollino.main line 650
+    self.centro.cenas[indice].portal(N=sala.cenas[indice]) if sala else None
+AttributeError: 'str' object has no attribute 'cenas'
+'''},
