daab048154a7b5ee111780526e648c042dbe1643
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 10:24:38 -0300

    Saved sara/__log__.py at Timestamp: 2019-09-04 13:24:37

--- sara/__log__.py
+++ sara/__log__.py
@@ -219,3 +219,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Wed Sep 04 2019 10:24:37.247 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 14
+  ana_maria=Elemento(img=ANA_MARIA)
+  ^
+IndentationError: expected an indented block
+'''},
