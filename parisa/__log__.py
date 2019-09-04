e008aad6b2d515042b5b15498fe5d2d5950a1259
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 10:17:27 -0300

    Created parisa/__log__.py at Timestamp: 2019-09-04 13:17:27

---
+++ parisa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Sep 04 2019 10:17:30.647 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  ifni infbn inrinfb o9ro imo
+        ^
+SyntaxError: invalid syntax
+'''},
