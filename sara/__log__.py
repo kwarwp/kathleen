f852609a8c7b07f1a5bab7728f73bf7f52c4e0ca
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 10:19:22 -0300

    Saved sara/__log__.py at Timestamp: 2019-09-04 13:19:22

--- sara/__log__.py
+++ sara/__log__.py
@@ -205,3 +205,10 @@ AttributeError: 'str' object has no attribute 'cenas'
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Wed Sep 04 2019 10:19:24.695 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 14
+  ana_maria=Elemento(img=ANA_MARIA)
+  ^
+IndentationError: expected an indented block
+'''},
