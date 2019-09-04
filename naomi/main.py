be5209b96d8f177c5e36947ccbb3ecf8f3f589b0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 11:03:04 -0300

    Saved naomi/main.py at Timestamp: 2019-09-04 14:03:03

--- naomi/main.py
+++ naomi/main.py
@@ -1,7 +1,7 @@
 # kathleen.naomi.main.py
 from _spy.vittolino.main import INVENTÁRIO as inv 
 from _spy.vitolli.main import Cena, Elemento, Labirinto 
-Ana_Maria= 
+Ana_Maria= "http://2.bp.blogspot.com/-Mbt4S17B-t4/VRqlGTeVNiI/AAAAAAAAFJo/W_pk8JFqp-U/s1600/Bow%2BDoll.png"
 Dr_Zuckman= "http://copycor.com.br/v2/wp-content/uploads/2016/04/boneco.png"
 Sala 1 = "https://upload.wikimedia.org/wikipedia/commons/8/8a/Chapada_Diamantina_Cen%C3%A1rio.JPG"
 SALA 2 = "http://www.conxrede.com.br/wp-content/uploads/2015/02/02-CENARIO-DUO-REDARIO.jpg"
@@ -9,12 +9,12 @@ SALA 3 = "https://1.bp.blogspot.com/-yJ_uAJKWEDw/T-SvdzMdq2I/AAAAAAAAAwE/-a6zCZH
 SALA 4=  "http://4.bp.blogspot.com/-lexcUCs4Ax4/T-StJYJUEEI/AAAAAAAAAvc/dwgTG6G4HLs/s1600/ESCENARIOS.jpg"
 
 Class Jesus ();
-def _init_main (self)
-ana_maria= Elemento (img= Ana_Maria)
-dr_zuckman= Elemento (img= Dr_Zuckman)
-lugar = Labirinto (n= SALA 1, l=SALA 2, o= SALA 3, s= SALA 4)
-ana_maria.entra(lugar.norte)
-dr_zuckman.entra( lugar.norte)
-lugar.norte.vai()
+		def _init_main (self)
+		ana_maria= Elemento (img= Ana_Maria)
+		dr_zuckman= Elemento (img= Dr_Zuckman)
+		lugar = Labirinto (n= SALA 1, l=SALA 2, o= SALA 3, s= SALA 4)
+		ana_maria.entra(lugar.norte)
+		dr_zuckman.entra( lugar.norte)
+		lugar.norte.vai()
 Jesus ()
 
