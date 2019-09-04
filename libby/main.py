9b1010f392d40a855aa257c0ecac036672948475
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 10:21:31 -0300

    Saved libby/main.py at Timestamp: 2019-09-04 13:21:31

--- libby/main.py
+++ libby/main.py
@@ -12,18 +12,18 @@ SALA1= "img norte LABORATORIO"
 SALA2= "img leste HOSPITAL"
 SALA3= "img oeste BIBLIOTECA"
 SALA4= "img sul LAB_INFORMATICA"
-Class HAHA():
-  def __init__(self):
- ana_maria= Elemento(img ANA_MARIA)
- dr_zuckman= Elemento(img DR_ZUCKMAN)
- cofre= Elemento(img COFRE)
- laboratorio= Cena(img LABORATORIO)
- hospital= Cena(img HOSPITAL)
- biblioteca= Cena(img BIBLIOTECA)
- lab_informatica= Cena(img LAB_INFORMATICA)
- hehe= LABIRINTO (norte= SALA1,leste= SALA2,oeste= SALA3,sul= SALA4)
- ana_maria.entra(hehe.norte)
- dr_zuckman.entra(hehe.norte)
- hehe.norte.vai()
+class HAHA():
+        def __init__(self):
+    ana_maria= Elemento(img ANA_MARIA)
+    dr_zuckman= Elemento(img DR_ZUCKMAN)
+     cofre= Elemento(img COFRE)
+    laboratorio= Cena(img LABORATORIO)
+     hospital= Cena(img HOSPITAL)
+    biblioteca= Cena(img BIBLIOTECA)
+    lab_informatica= Cena(img LAB_INFORMATICA)
+    hehe= LABIRINTO (norte= SALA1,leste= SALA2,oeste= SALA3,sul= SALA4)
+    ana_maria.entra(hehe.norte)
+    dr_zuckman.entra(hehe.norte)
+    hehe.norte.vai()
 
 HAHA()
