c1be5eede2e2f34e33d24f12b83df969c1990aa2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-04 10:19:02 -0300

    Saved sara/main.py at Timestamp: 2019-09-04 13:19:01

--- sara/main.py
+++ sara/main.py
@@ -10,16 +10,16 @@ SALA1="http://lorempixel.com/800/600/city"
 SALA2="http://lorempixel.com/800/600/city"
 
 class CHOCOLATE():
-    #def __init__(self):
-        ana_maria=Elemento(img=ANA_MARIA)
-        dr_duckman= Elemento(img=DR_DUCKMAN)
-        laboratorio= Cena(img=LABORATORIO)
-        hospital= Cena(img=HOSPITAL)
-        sala1= Cena(img=SALA1)
-        sala2= Cena(img=SALA2)
-        papel= Labirinto(n=SALA1,s=SALA2)
-        ana_maria.entra(papel.norte)
-        dr_duckman.entra(papel.norte)
-        papel.norte.vai()
+    def __init__(self):
+    ana_maria=Elemento(img=ANA_MARIA)
+    dr_duckman= Elemento(img=DR_DUCKMAN)
+    laboratori= Cena(img=LABORATORIO)
+    hospital= Cena(img=HOSPITAL)
+    sala1= Cena(img=SALA1)
+    sala2= Cena(img=SALA2)
+    papel= Labirinto(n=SALA1,l=SALA2)
+    ana_maria.entra(papel.norte)
+    dr_duckman.entra(papel.norte)
+    papel.norte.vai()
     
 CHOCOLATE() 
