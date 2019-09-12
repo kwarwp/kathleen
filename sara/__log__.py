06bef1971f2736645fb4a0f9034b2217eb0908e2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-12 11:27:20 -0300

    Saved sara/__log__.py at Timestamp: 2019-09-12 14:27:20

--- sara/__log__.py
+++ sara/__log__.py
@@ -233,3 +233,25 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Thu Sep 12 2019 11:26:37.167 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 12
+    class CHOCOLATE():
+  module <module> line 20
+    papel= Labirinto(n=SALA1,s=SALA2)
+  module _spy.vitollino.main line 646
+    self.lb()
+  module _spy.vitollino.main line 650
+    self.centro.cenas[indice].portal(N=sala.cenas[indice]) if sala else None
+AttributeError: 'str' object has no attribute 'cenas'
+'''},
