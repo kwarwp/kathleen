24c15ea510fc0eaae8f60b4d99a3fa8b096e625b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-16 19:33:13 -0300

    Saved naomi/__log__.py at Timestamp: 2019-09-16 22:33:13

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -129,3 +129,10 @@ SyntaxError: invalid syntax
          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Sep 16 2019 19:33:13.627 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  class Jesus();
+               ^
+SyntaxError: invalid syntax
+'''},
