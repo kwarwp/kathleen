c693c31649aa09ba8c20caa8749009cf342f5e10
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-16 19:48:10 -0300

    Saved naomi/__log__.py at Timestamp: 2019-09-16 22:48:10

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -206,3 +206,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Sep 16 2019 19:48:10.146 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 15
+  class OI 
+            ^
+SyntaxError: invalid syntax
+'''},
