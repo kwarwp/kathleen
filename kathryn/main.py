75daf35e359ae74dd7359314254db7b913a66a6a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-16 19:44:54 -0300

    Saved kathryn/main.py at Timestamp: 2019-09-16 22:44:54

--- kathryn/main.py
+++ kathryn/main.py
@@ -6,5 +6,4 @@ print(x)
 #listas
 casa = [1,"hortencia",[22,16,13]]
 print(casa,[1])
-joelho = "https://img.freepik.com/fotos-gratis/praia-e-mar_74190-2371.jpg?size=626&ext=jpg"
-print ( "não aguento mais estudar" , joelho )
+
