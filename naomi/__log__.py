6d36d90ab481e076c58f0a57e4b45df9c16a107a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-16 19:53:11 -0300

    Saved naomi/__log__.py at Timestamp: 2019-09-16 22:53:10

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -227,3 +227,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Sep 16 2019 19:53:10.550 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 11
+  boneca.entra(homem)
+  ^
+IndentationError: unexpected indent
+'''},
