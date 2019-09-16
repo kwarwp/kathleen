9cf0414f2251c72e4b1a3983553194d6ae25b43c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-16 19:40:49 -0300

    Saved naomi/main.py at Timestamp: 2019-09-16 22:40:49

--- naomi/main.py
+++ naomi/main.py
@@ -1,20 +1,13 @@
 # kathleen.naomi.main.py
-from _spy.vittolino.main import INVENTÁRIO as inv 
-from _spy.vitolli.main import Cena, Elemento, Labirinto 
-Ana_Maria= "http://2.bp.blogspot.com/-Mbt4S17B-t4/VRqlGTeVNiI/AAAAAAAAFJo/W_pk8JFqp-U/s1600/Bow%2BDoll.png"
-Dr_Zuckman= "http://copycor.com.br/v2/wp-content/uploads/2016/04/boneco.png"
-Sala 1 = "https://upload.wikimedia.org/wikipedia/commons/8/8a/Chapada_Diamantina_Cen%C3%A1rio.JPG"
-SALA 2 = "http://www.conxrede.com.br/wp-content/uploads/2015/02/02-CENARIO-DUO-REDARIO.jpg"
-SALA 3 = "https://1.bp.blogspot.com/-yJ_uAJKWEDw/T-SvdzMdq2I/AAAAAAAAAwE/-a6zCZHweig/s1600/ESCENARIOS+%285%29.jpg"
-SALA 4=  "http://4.bp.blogspot.com/-lexcUCs4Ax4/T-StJYJUEEI/AAAAAAAAAvc/dwgTG6G4HLs/s1600/ESCENARIOS.jpg"
+from _spy.vitollino.main import INVENTARIO as inv
+from _spy.vitollino.main import Cena, Elemento, Texto 
+HOMEM = "https://3.bp.blogspot.com/-xsgQSKi0xHg/V1IMCh9vlqI/AAAAAAAAEDw/S81m4JHTNdYOrdQvm_GLEXt2eqVr8NoNgCLcB/s1600/pngg.png"
+BONECA = "https://images.vexels.com/media/users/3/134449/isolated/preview/fd3c681037ff454277a744562ae002cd-boneca-da-menina-dos-desenhos-animados-by-vexels.png"
 
-Class Jesus ();
-		def _init_main (self)
-		ana_maria= Elemento (img= Ana_Maria)
-		dr_zuckman= Elemento (img= Dr_Zuckman)
-		lugar = Labirinto (n= SALA 1, l=SALA 2, o= SALA 3, s= SALA 4)
-		ana_maria.entra(lugar.norte)
-		dr_zuckman.entra( lugar.norte)
-		lugar.norte.vai()
-Jesus ()
-
+class OI ():
+		#def _init_main (self)
+		 homem = Cena (img= HOMEM)
+		 boneca = Elemnento (img= BONECA)
+            boneca.entra(homem)
+            boneca.vai()
+class OI 
