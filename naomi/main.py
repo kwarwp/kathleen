3d4fa5c7b3f0ac22b838d7ef7ffded8bac7e3d9a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-16 19:47:42 -0300

    Saved naomi/main.py at Timestamp: 2019-09-16 22:47:42

--- naomi/main.py
+++ naomi/main.py
@@ -2,13 +2,13 @@
 from _spy.vitollino.main import INVENTARIO as inv
 from _spy.vitollino.main import Cena, Elemento 
 HOMEM = "https://3.bp.blogspot.com/-xsgQSKi0xHg/V1IMCh9vlqI/AAAAAAAAEDw/S81m4JHTNdYOrdQvm_GLEXt2eqVr8NoNgCLcB/s1600/pngg.png"
-BONECA = "https://images.vexels.com/media/users/3/134449/isolated/preview/fd3c681037ff454277a744562ae002cd-boneca-da-menina-dos-desenhos-animados-by-vexels.png"
+#BONECA = "https://images.vexels.com/media/users/3/134449/isolated/preview/fd3c681037ff454277a744562ae002cd-boneca-da-menina-dos-desenhos-animados-by-vexels.png"
 
 class OI ():
 		#def _init_main (self)
 		 homem = Cena (img= HOMEM)
+		 #boneca = Elemnento (img= BONECA)
+            #boneca.entra(homem)
+           # boneca.vai()
             homem.vai()
-		 boneca = Elemnento (img= BONECA)
-            boneca.entra(homem)
-            boneca.vai()
 class OI 
