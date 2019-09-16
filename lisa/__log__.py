f712d1b81c2fed7d7d7279a772e3b2f13c931f01
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-16 19:39:42 -0300

    Created lisa/__log__.py at Timestamp: 2019-09-16 22:39:42

---
+++ lisa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Sep 16 2019 19:39:40.750 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  def __ int __(self):
+          ^
+SyntaxError: invalid syntax
+'''},
