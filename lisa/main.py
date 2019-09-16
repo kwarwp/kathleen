8fd24ab36443495a4c472eab191bc505372e8e0e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-16 19:40:47 -0300

    Saved lisa/main.py at Timestamp: 2019-09-16 22:40:47

--- lisa/main.py
+++ lisa/main.py
@@ -1,11 +1,11 @@
 # kathleen.lisa.main.py
 from _spy.vitolino.main import Cena,Elemento,Labirinto
 from _spy.vitolino.main import inventario as inv
-PRAIA=...
-BOLA=...
+PRAIA= "https://img.freepik.com/fotos-gratis/praia-e-mar_74190-2371.jpg?size=626&ext=jpg"
+BOLA= "https://3.bp.blogspot.com/-xsgQSKi0xHg/V1IMCh9vlqI/AAAAAAAAEDw/S81m4JHTNdYOrdQvm_GLEXt2eqVr8NoNgCLcB/s1600/pngg.png"
 
 class PARTY():
-      def __ int __(self):
+      #def __ int __(self):
         praia=Cena(img=PRAIA)
         bola=Elemento(img=BOLA)
         bola.entra(praia)
