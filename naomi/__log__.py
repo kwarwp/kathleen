96a68ee3b7c05d1643e24d9afb200b201cfa5432
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-16 19:31:58 -0300

    Saved naomi/__log__.py at Timestamp: 2019-09-16 22:31:58

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -101,3 +101,10 @@ SyntaxError: trailing comma not allowed without surrounding parentheses
                                                                   ^
 SyntaxError: trailing comma not allowed without surrounding parentheses
 '''},
+{'date': 'Mon Sep 16 2019 19:31:57.987 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  Class jesus ();
+         ^
+SyntaxError: invalid syntax
+'''},
