52ab1fb26b6046785cf15580e9035d22c7770a7f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-17 16:03:55 -0300

    Saved sarah/__log__.py at Timestamp: 2019-09-17 19:03:54

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -90,3 +90,33 @@ KeyError: img
     imagem, kwargs = (imagem, {}) if isinstance(imagem, str) \
 KeyError: img
 '''},
+{'date': 'Tue Sep 17 2019 16:04:02.591 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 6
+    class FELICIDADE():
+  module <module> line 7
+    		frio = Cena ( img= FRIO, style= dict( 
+  module _spy.vitollino.main line 782
+    Cena.c(**kwargs)
+  module _spy.vitollino.main line 825
+    imagem, kwargs = (imagem, {}) if isinstance(imagem, str) \
+KeyError: img
+'''},
