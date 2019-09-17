79f83f90695ac3f38742cc849f14fdfa6cf8a8d6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-17 15:59:53 -0300

    Created sarah/__log__.py at Timestamp: 2019-09-17 18:59:53

---
+++ sarah/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Sep 17 2019 15:59:59.970 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  from _spy.vittollino.main.import
+                                  ^
+SyntaxError: trailing comma not allowed without surrounding parentheses
+'''},
