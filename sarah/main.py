b0f095d1c2cc7a0d2156aa6aa021749be33d1d26
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-17 16:01:38 -0300

    Saved sarah/main.py at Timestamp: 2019-09-17 19:01:38

--- sarah/main.py
+++ sarah/main.py
@@ -1,6 +1,6 @@
 # kathleen.sarah.main.py
-from _spy.vitollino.main.import Cena , Elemento , Texto
-#from _spy.vittollino.main.import
+from _spy.vitollino.main import Cena , Elemento , Texto
+from _spy.vitollino.main import INVENTARIO as inv 
 FRIO = "https://cdn.pixabay.com/photo/2014/12/15/03/09/house-568557_960_720.jpg"
 BILLIE = "https://p1.hiclipart.com/preview/871/471/638/billie-eilish-woman-wearing-brown-and-red-bobble-jacket-png-clipart.jpg"
 class FELICIDADE():
