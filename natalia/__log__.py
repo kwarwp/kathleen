e5a5e91292ebd5e6e919af5eaa7c81110a8dd2e0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-17 15:55:42 -0300

    Created natalia/__log__.py at Timestamp: 2019-09-17 18:55:42

---
+++ natalia/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Sep 17 2019 15:57:16.407 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  from _spy.vitollino.main import
+                                 ^
+SyntaxError: trailing comma not allowed without surrounding parentheses
+'''},
