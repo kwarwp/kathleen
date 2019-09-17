e587e6dadeeb40946339a663ab065fe6ee4415d1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-17 16:05:47 -0300

    Saved adda/main.py at Timestamp: 2019-09-17 19:05:47

--- adda/main.py
+++ adda/main.py
@@ -1,1 +1,8 @@
-# kathleen.adda.main.py
+# kathleen.adda.main.py 
+from _spy.vitollino.main import Cena,Elemento,Texto
+from _spy.vitolli.main import INVENTARIO as inv
+HOTEL="https://s3.amazonaws.com/img.portalmarcossantos.com.br/wp-content/uploads/2019/06/16035159/record-cenarios.jpg"
+TEO="https://icon2.kisspng.com/20180714/vqe/kisspng-watch-dogs-2-aiden-pearce-character-aiden-pearce-5b4a964ba77a43.716457391531614795686.jpg" 
+class MARCHELLO():
+		hotel=cena(img=HOTEL)
+		teo=elemento(img=TEO)
