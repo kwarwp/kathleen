92984d33c9df076b7460f373d9b02e54fb3d540c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-17 15:55:50 -0300

    Saved natalia/main.py at Timestamp: 2019-09-17 18:55:49

--- natalia/main.py
+++ natalia/main.py
@@ -1,8 +1,11 @@
-# kathleen.natalia.main.py
-from _spy.vitollino.main import Cena, Elemento
-from _spy.vitollino.main import Inventario as inv
-CEN="https://4.bp.blogspot.com/-qOhCgMJvsPY/VxkW1ZBBrZI/AAAAAAAANwA/GNtWUUpfXg8rYfDBPF2SSadkF64CLRuMQCLcB/s1600/bg%2B04.png"
-class OLA ():
-	cen= Cena(img=CEN)
-	cen.vai()
-OLA()
+# kathleen.natalia.main.
+from _spy.vitollino.main import Cena, Elemnento
+from _spy.vitollino.main import
+TERROR="https://i.pinimg.com/originals/3c/b3/dc/3cb3dcb19461b496f4f78611c2483593.jpg"
+IT="https://kanto.legiaodosherois.com.br/w760-h398-gnw-cfill-q80/wp-content/uploads/2019/07/legiao_6Uiy7PnVglSBbp_XehZYw3Dd18W0LETcakrsIKjuN5.png.jpeg"
+class  CARLOS ():
+		terror = Cena (img=TERROR)
+		it  = Elemento ( img = IT)
+		it.entra(terror)
+		terror.vai()
+CARLOS()
