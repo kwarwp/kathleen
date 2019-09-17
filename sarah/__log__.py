cc69e3f514e7c57b12fa9f3f36d8b73d94125ec5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-17 16:00:13 -0300

    Saved sarah/__log__.py at Timestamp: 2019-09-17 19:00:12

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -6,3 +6,9 @@
                                   ^
 SyntaxError: trailing comma not allowed without surrounding parentheses
 '''},
+{'date': 'Tue Sep 17 2019 16:00:20.767 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+Exception: Cannot read property 'Cena' of undefined
+  module '$exec_461' line 2
+from _spy.vitollino.main.import Cena , Elemento , Texto
+'''},
