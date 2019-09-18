fbc14d4fc7e4d1525670b51739e16aef4d51ae74
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-18 17:47:29 -0300

    Created ruzwana/main.py at Timestamp: 2019-09-18 20:47:29

---
+++ ruzwana/main.py
@@ -0,0 +1,9 @@
+# kathleen.ruzwana.main.py
+from _spy.vittolino.main import Cena, Elemento , Labirinto
+from _spy,vittolino.main import Ineventario as inv
+JACK=" FT"
+EDGAR=""
+AJUDANTE=""
+LADRAO=""
+CAES=""
+
