0ab764f2b3a177850cbd0a0b1def481f9da31ccf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-20 23:41:46 -0300

    Saved sara/main.py at Timestamp: 2019-09-21 02:41:46

--- sara/main.py
+++ sara/main.py
@@ -1,6 +1,9 @@
 # kathleen.sara.main.py
-from _spy.vitollino.main import Cena, Elemento, Labirinto
+from _spy.vitollino.main import Cena, Elemento, Labirinto, STYLE, Sala
 from _spy.vitollino.main import INVENTARIO as inv
+
+STYLE["width"] = 800
+STYLE["height"] = "600px"
 
 ANA_MARIA="http://lorempixel.com/800/600/people/1"
 DR_DUCKMAN="http://lorempixel.com/800/600/people/2"
@@ -10,16 +13,16 @@ SALA1="http://lorempixel.com/800/600/city"
 SALA2="http://lorempixel.com/800/600/city"
 
 class CHOCOLATE():
-    def __init__(self):
-    ana_maria=Elemento(img=ANA_MARIA)
-    dr_duckman= Elemento(img=DR_DUCKMAN)
-    laboratorio= Cena(img=LABORATORIO)
-    hospital= Cena(img=HOSPITAL)
-    sala1= Cena(img=SALA1)
-    sala2= Cena(img=SALA2)
-    papel= Labirinto(n=SALA1,s=SALA2)
-    ana_maria.entra(papel.norte)
-    dr_duckman.entra(papel.norte)
-    papel.norte.vai()
+   # def __init__(self):
+        ana_maria=Elemento(img=ANA_MARIA)
+        dr_duckman= Elemento(img=DR_DUCKMAN)
+        laboratorio= Cena(img=LABORATORIO)
+        hospital= Cena(img=HOSPITAL)
+        sala1= Cena(img=SALA1)
+        sala2= Cena(img=SALA2)
+        papel= Labirinto(n=SALA1,s=SALA2)
+        ana_maria.entra(papel.norte)
+        dr_duckman.entra(papel.norte)
+        papel.norte.vai()
     
 CHOCOLATE() 
