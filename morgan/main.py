8bd6bb178615f2c30a84e79b3239b89bc71f9020
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-22 10:01:09 -0300

    Saved morgan/main.py at Timestamp: 2019-09-22 13:01:09

--- morgan/main.py
+++ morgan/main.py
@@ -3,12 +3,49 @@ from _spy.vitollino.main import Cena, Elemento, Labirinto
 from _spy.vitollino.main import Inventario as inv
 
 
-ARENA = "https://i.pinimg.com/originals/79/42/7f/79427f813a3e0c39eb834ed07a7e96af.jpg"
-KATNISS = "https://1.bp.blogspot.com/-uIYoL6p-M54/V1NLGwpwgYI/AAAAAAAAFPM/j5-M-jR7xdkyHTN6tgCp0DoT4TUF8ZuEwCKgB/s640/31.png"
-class HGAMES():
+GABINETE = 
+DELEGACIA = 
+FESTA = 
+CEMITERIO =
+DELEGADO =
+MORDOMO =
+POLITICO = 
+HOMEM =
+PASSISTA =
+
+
+class TORDO():
       #def __ int __(self):
-        arena = Cena ( img = ARENA )
-        katniss = Elemento ( img = KATNISS )
-        katniss.entra(arena)
-        arena.vai()
-HGAMES()
+        festa = Cena ( img = FESTA  )
+        gabinete = Cena ( img = GABINETE )
+        delegacia = Cena ( img = DELEGACIA )
+        cemiterio = Cena ( img = CEMITERIO )
+        delegado = Elemento ( img = DELEGADO )
+        mordomo = Elemento ( img = MORDOMO )
+        politico = Elemento ( img = POLITICO )
+        homem = Elemento ( img = HOMEM )
+        passista = Elemento ( img = PASSISTA )
+        lugar = Labirinto (n= GABINETE, l= FESTA, o= DELEGACIA, s= CEMITERIO)
+        
+        delegado.entra(lugar.norte)
+        mordomo.entra(lugar.norte)
+        politico.entra(lugar.norte)
+        lugar.norte.vai()
+        
+        passista.entra(lugar.leste)
+        mordomo.entra(lugar.leste)
+        homem.entra(lugar.leste)
+        lugar.leste.vai()
+        
+        homem.entra(lugar.oeste)
+        lugar.oeste.vai()
+        
+        homem.entra(lugar.sul)
+        mordomo.entra(lugar.sul)
+        politico.entra(lugar.sul)
+        delegado.entra(lugar.sul)
+        lugar.sul.vai()
+        
+        TORDO():
+        
+        
