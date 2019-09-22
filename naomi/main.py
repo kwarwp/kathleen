c7cbc92aa17dcad052e126d114854336c0e01641
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-09-22 10:38:12 -0300

    Saved naomi/main.py at Timestamp: 2019-09-22 13:38:12

--- naomi/main.py
+++ naomi/main.py
@@ -13,4 +13,15 @@ class OI():
 		cena.vai()
            
          
+         
 OI() 
+GABINETE = "https://i.imgur.com/dp2Gnak.png"
+DELEGACIA = "https://image.flaticon.com/icons/png/512/551/551512.png"
+FESTA = "https://img.freepik.com/vetores-gratis/silhueta-de-um-publico-de-festa_1048-9714.jpg?size=626&ext=jpg"
+CEMITERIO = "https://images.vexels.com/media/users/3/130673/isolated/preview/c58973769fe0badffc8ec348fe555f20---cone-do-cemit--rio-by-vexels.png"
+DELEGADO = "https://images.vexels.com/media/users/3/158713/isolated/preview/7ad4403a507c6cf10d9d34093f6fa690-ilustra----o-uniforme-de-policial-by-vexels.png"
+MORDOMO = "http://clipart.coolclips.com/480/vectors/tf05165/CoolClips_vc009999.png"
+POLITICO = "https://pngimage.net/wp-content/uploads/2018/06/politico-png-6.png"
+HOMEM = "https://thumbs.dreamstime.com/t/homem-negro-novo-que-sorri-contra-o-fundo-branco-96739013.jpg"
+PASSISTA = "https://images.vexels.com/media/users/3/151285/isolated/preview/43ea02778621af6aca3631f2ba619bf5-mulher-carnaval-dan--ar-by-vexels.png"
+
