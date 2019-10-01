4a675e9b5d6a3829d6e8865ecee191bbe0dda68b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-01 19:03:52 -0300

    Saved roxanne/__log__.py @2019-10-01 22:03

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -117,3 +117,23 @@ NameError: name 'dr_zukman' is not defined
     porta.entra(congresso.norte)
 AttributeError: 'porta' object has no attribute 'entra'
 '''},
+{'date': 'Tue Oct 01 2019 19:03:51.0 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 54
+    Cenas()
+  module <module> line 49
+    Porta().entra(congresso.norte)
+  module <module> line 38
+    dragger=dict(Aninha=self.entra_porta)
+AttributeError: 'Porta' object has no attribute 'entra_porta'
+'''},
