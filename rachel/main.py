30342a4caaa0e2137bdf6aeef60aff97a419a263
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-06 10:28:20 -0300

    Saved rachel/main.py @2019-10-06 13:28

--- rachel/main.py
+++ rachel/main.py
@@ -1,9 +1,1 @@
-from browser import document, html
-URL='http://lorempixel.com/300/city/5'
-pydom=document['pydiv']
-pydom.html=''
-pydom.html='Hello World'
-div1=html.DIV()
-pydom<=div1
-imagem1=html.IMG(src=URL, width=300, height='300px')
-div1<=imagem1
+# kathleen.rachel.main.py
