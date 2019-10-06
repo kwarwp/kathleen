a9512177b6f96c7acff852929fde7801e550fcd6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-06 10:33:42 -0300

    Saved sarah/main.py @2019-10-06 13:33

--- sarah/main.py
+++ sarah/main.py
@@ -1,12 +1,1 @@
 # kathleen.sarah.main.py
-from _spy.vitollino.main import Cena , Elemento , Texto
-from _spy.vitollino.main import INVENTARIO as inv 
-FRIO = "https://cdn.pixabay.com/photo/2014/12/15/03/09/house-568557_960_720.jpg"
-BILLIE = "https://p1.hiclipart.com/preview/871/471/638/billie-eilish-woman-wearing-brown-and-red-bobble-jacket-png-clipart.jpg"
-class FELICIDADE():
-		frio = Cena ( img= FRIO)
-		billie = Elemento ( img= BILLIE)
-		billie.entra(frio)
-		frio.vai()
-
-FELICIDADE()
