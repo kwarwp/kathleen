bbb17bce6486407d5c352f19d7763a0c5377621d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-07 15:37:12 -0300

    Saved soraya/__log__.py @2019-10-07 18:37

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -6,3 +6,10 @@
       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Oct 07 2019 15:37:14.216 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  from-spy.vitolino.main import cena, elemento, labirinto
+      ^
+SyntaxError: invalid syntax
+'''},
