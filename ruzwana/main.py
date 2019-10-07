780e6df32b677b0cf7be03f9a43b0853f92ed1f0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-07 16:59:57 -0300

    Saved ruzwana/main.py @2019-10-07 19:59

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -1,3 +1,46 @@
 # kathleen.ruzwana.main.py
-
-
+from _spy.vittolino.main import Cena, Elemento , Labirinto
+from _spy.vittolino.main import inventario as inv
+DOENTE=""
+AJUDANTE=""
+CIENTISTA=""
+LADRAO=""
+CAES=""
+BIBLIOTECA=""
+CASA=""
+COMPUTADOR=""
+REMEDIO=""
+PORTA=""
+GUARDAS=""
+LIVRO=""
+LABORATORIO=""
+PREDIO=""
+TUNEL=""
+SAIDA=""
+LIXO=""
+MAPA=""
+ class leni():
+    doente= Elemento(img=DOENTE)
+    ajudante= Elemento(img=AJUDANTE)
+    cientista= Elemento(img=CIENTISTA)
+    ladrao= Elemento(img=LADRAO)
+    caes= Elemento(img=CAES)
+    guardas= Elemento(img=GUARDAS)
+    remedio= Elemento(img=REMEDIO)
+    mapa= Elemento(img=MAPA)
+    computador= Elemento(img=COMPUTADOR)
+    lixo= Elemento(img=LIXO)
+    porta= Elemento(img=PORTA)
+    livro= Elemento(img=LIVRO)
+    biblioteca= Cena(img=BIBLIOTECA)
+    casa= Cena (img=CASA)
+    laboratorio= Cena(img=LABORATORIO)
+    predio= Cena(img=PREDIO)
+    tunel= Cena(img=TUNEL)
+    saida= Cena(img=SAIDA)
+      doente.entra(casa)
+        ajudante.entra(casa)
+        ladrao.entra(casa)
+        ajudante.entra(biblioteca)
+        doente.entra(biblioteca)
+        
