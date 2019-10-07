09d894595c91ffe7710f1f74fee760290cac4aae
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-07 17:15:44 -0300

    Saved danae/main.py @2019-10-07 20:15

--- danae/main.py
+++ danae/main.py
@@ -1,1 +1,14 @@
 # kathleen.danae.main.py
+from _spy.vittolino.main import Cena, Elemento , Labirinto
+from _spy.vittolino.main import inventario as inv
+CIENTISTA=""
+AJUDANTE=""
+LABORATORIO=""
+JARDIM=""
+CAIXADEMUSICA=""
+HOLOGRAMA=""
+JOGADOR=""
+HOSPITAL=""
+ANTIDOTO=""
+PAREDE=""
+SETASFLORESCENTS=""
