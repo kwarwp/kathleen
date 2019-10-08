e8599760623fb2a2275630492da3ce6d61d6aee0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 13:29:50 -0300

    Saved parisa/__log__.py @2019-10-08 16:29

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -171,3 +171,25 @@ NameError: name 'SALA2' is not defined
     sala1 = Cena(img = INICIO)
 NameError: name 'INICIO' is not defined
 '''},
+{'date': 'Tue Oct 08 2019 10:30:05.711 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 6
+    class OI():
+  module <module> line 8
+    mensagens=Codigo(cena=inicio, topo="Olá, nesse jogo você irá lidar com varios desafios........ TERMINAR A CONSTRUÇÃO", codigo="lorem ipsum sic dolor") 
+NameError: name 'inicio' is not defined
+'''},
