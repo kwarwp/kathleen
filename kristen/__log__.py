5adc8f374e9cd505ff1660cc56c0e1587dda42b6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 13:31:30 -0300

    Saved kristen/__log__.py @2019-10-08 16:31

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -232,3 +232,10 @@ SyntaxError: invalid syntax
                                                                       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Oct 08 2019 13:31:30.760 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 32
+  fio2=Labirinto (L=salinha,S=estante,O=
+                                                                      ^
+SyntaxError: invalid syntax
+'''},
