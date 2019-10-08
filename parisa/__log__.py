96827c6462020cc355fbb296e29bbb1714ae9d33
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 13:30:11 -0300

    Saved parisa/__log__.py @2019-10-08 16:30

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -193,3 +193,33 @@ NameError: name 'INICIO' is not defined
     mensagens=Codigo(cena=inicio, topo="Olá, nesse jogo você irá lidar com varios desafios........ TERMINAR A CONSTRUÇÃO", codigo="lorem ipsum sic dolor") 
 NameError: name 'inicio' is not defined
 '''},
+{'date': 'Tue Oct 08 2019 10:30:26.800 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 6
+    class OI():
+  module <module> line 15
+    todos = Labirinto( n= SALA1, s= SALA2)
+  module _spy.vitollino.main line 839
+    self.lb()
+  module _spy.vitollino.main line 843
+    self.centro.cenas[indice].portal(N=sala.cenas[indice]) if sala else None
+AttributeError: 'str' object has no attribute 'cenas'
+'''},
