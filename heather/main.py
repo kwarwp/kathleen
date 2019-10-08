e15482a51ae3619865c3d990ccf2041d1b7aa25e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 12:02:16 -0300

    Created heather/main.py @2019-10-08 15:02

---
+++ heather/main.py
@@ -0,0 +1,11 @@
+# kathleen.heather.main.py
+from _spy.vittorino.main import Cena, Elemento, Labirinto, Texto
+from _spy.vittorino.main import inventario as inv
+ESCADA="https://png.pngtree.com/png-clipart/20190121/ourmid/pngtree-cartoon-hand-drawn-watercolor-stair-design-cartoon-hand-painted-watercolor-png-image_517547.jpg"
+AJUDANTE="https://png.pngtree.com/element_origin_min_pic/16/06/14/16575fc06949b28.jpg"
+GARCAS="https://img1.gratispng.com/20180131/lye/kisspng-bird-crane-egret-bird-5a727ebde23183.4981214015174529899265.jpg"
+REDE="www.entomologiaonline.com.br/image/cache/data/bug_catcher-350x430.jpg"
+LABORATORIO="http://blog.homyquimica.com.br/wp-content/uploads/2018/02/homy-capa-07-02-1.png"
+class SEILACARA()
+garrafa=Cena (
+
