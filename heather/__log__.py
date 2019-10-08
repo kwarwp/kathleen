260a0d413c6e107c68c6d91e746208427d137f4d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 15:08:43 -0300

    Created heather/__log__.py @2019-10-08 18:08

---
+++ heather/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Oct 08 2019 12:08:58.404 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 12
+  class SEILACARA()
+                    ^
+SyntaxError: invalid syntax
+'''},
