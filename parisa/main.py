cd937835c1d5d4d2c896f342b56d5bb5198cf2f2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 13:29:44 -0300

    Saved parisa/main.py @2019-10-08 16:29

--- parisa/main.py
+++ parisa/main.py
@@ -4,7 +4,7 @@ from _spy.vittolino.main import INVENTARIO as inv
 SALA1 = "https://www.loskatchorros.com.br/forum/uploads/monthly_2017_08/maxresdefault.jpg.3845d9d72470fe4e52aa779cf96871b5.jpg"
 SALA2 = "http://www.colegiohoje.com.br/img/predioEscola.png"
 class OI():
-    sala1 = Cena(img = INICIO)
+    sala1 = Cena(img = SALA1)
     mensagens=Codigo(cena=inicio, topo="Olá, nesse jogo você irá lidar com varios desafios........ TERMINAR A CONSTRUÇÃO", codigo="lorem ipsum sic dolor") 
     sala1.vai()
     mensagens.vai()
