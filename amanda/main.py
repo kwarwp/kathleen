a0903966706d22a316aff3e294209d95fec0c04f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 13:12:14 -0300

    Created amanda/main.py @2019-10-08 16:12

---
+++ amanda/main.py
@@ -0,0 +1,29 @@
+# kathleen.amanda.main.py
+from _spy.vittolino.main import Cena,Elemento,Labarinto,Texto
+from _spy.vittolino.main import Inventario as inv
+
+CASA = "img"
+SALA = "img"
+ZEZINHO = "img"
+CIENTISTAMLK = "img"
+LABORATORIO = "img"
+ROSALINDA = "img"
+COFRE = "img"
+MAPA = "img"
+CORREDOR = "img"
+PAPEL = "img"
+HEREDOGRAMA1 = "img"
+HEREDOGRAMA2 = "img"
+PAPEL = "img"
+QUEBRACABECA = "img"
+
+
+class heredograma ():
+
+    casa=Cena(img=CASA)
+    corredor=Cena(img=CORREDOR)
+    sala=Cena(img=SALA)
+    laboratorio=Cena(img=LABORATORIO)
+    
+    
+    
