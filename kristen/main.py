f5a2b4dfe38b4c04a70b6818eeeed724c3977eb4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 08:07:59 -0300

    Saved kristen/main.py @2019-10-08 11:07

--- kristen/main.py
+++ kristen/main.py
@@ -1,3 +1,37 @@
-# kathleen.kristen.main.py
+# kathleen.kristen.main.PY
+from _spy.vitollino.main import Cena, Elemento,Labirinto
+from _spy.vitollino.main import INVENTARIO as inv
+ZEZINHO="
+CASA="
+ROSALINDA="
+COFRE="
+LIVRO="
+CORREDOR="
+MONITOR=
+PORTINHA=
+LABORATORIO=
+DR_HAMSTER=
 
+class CARTA():
+    zeze= Elemento (img=ZEZINHO)
+    casinha= Cena (img=CASA)
+    rosa=Elemento (img=ROSALINDA)
+    cofre=Cena (img=COFRE)
+    livrinho=Elemento (img=LIVRO)
+    corredor=Cena(img=CORREDOR)
+    monitor=Elemento (img=MONITOR)
+    portinha=Cena (img=PORTA)
+    salinha=Cena (img=LABORATORIO)
+    dr=Elemento (img=DR_HAMSTER)
+    zeze.entra(casinha)
+    rosa.entra(casinha)
+    casinha.vai()
+    zeze.entra(corredor)
+    corredor.vai()
+    zeze.entra(salinha)
+    dr.entra(salinha)
+    salinha.vai()
+    rosa.entra(salinha)
+    salinha.vai()
     
+    CARTA()
