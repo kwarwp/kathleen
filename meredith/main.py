5514b05c0f887ef1097d6679a70480d03bda9fdc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 12:37:22 -0300

    Saved meredith/main.py @2019-10-08 15:37

--- meredith/main.py
+++ meredith/main.py
@@ -1,1 +1,14 @@
 # kathleen.meredith.main.py
+from _spy.vitollino.main import Cena, Texto
+
+
+class Opt:
+    def __init__(self):
+        self.c = c = Cena("http://lorempixel.com/400/200/")
+        self.t = t = Texto(c,"vai?", A="Vai A", B="Vai B")
+        t.mostra(c,"vai?", A="Vai A", B="Vai B")
+        t.vai()
+        self.f = f = Texto(c, t.POP.optou)
+        c.vai()
+        
+Opt()
