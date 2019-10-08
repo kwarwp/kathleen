ab7be8ce843bf89e630f562dea52ce9c735866e2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 13:38:41 -0300

    Saved parisa/main.py @2019-10-08 16:38

--- parisa/main.py
+++ parisa/main.py
@@ -6,21 +6,17 @@ SALA2 = "http://www.colegiohoje.com.br/img/predioEscola.png"
 SALA3 = "https://cdn.pixabay.com/photo/2017/04/07/19/16/infinity-blue-2211659_960_720.jpg"
 SALA4 = "https://cdn.pixabay.com/photo/2017/04/22/00/14/universe-2250310_960_720.jpg"
 class OI():
-    sala1 = Cena(img = SALA1)
-    mensagens=Codigo(cena=sala1, topo="Olá, nesse jogo você irá lidar com varios desafios........ TERMINAR A CONSTRUÇÃO", codigo="lorem ipsum sic dolor") 
-    sala1.vai()
-    mensagens.vai()
-    
-    sala2 = Cena(img = SALA2)
-    sala2.vai()
-    
-    sala3 = Cena(img = SALA3)
-    sala3.vai()
-    
-    sala4 = Cena(img = SALA4)
-    sala4.vai()
-    
-    todos = Labirinto( n= SALA1, s= SALA2, l= SALA3, o= SALA4)
-    
+    def __init__(self):
+        sala1 = Cena(img = SALA1)
+        mensagens=Codigo(cena=sala1, topo="Olá, nesse jogo você irá lidar com varios desafios........ TERMINAR A CONSTRUÇÃO", codigo="lorem ipsum sic dolor") 
+        sala1.vai()
+        mensagens.vai()
+        sala2 = Cena(img = SALA2)
+        sala3 = Cena(img = SALA3)
+        sala4 = Cena(img = SALA4)
+        sala4.vai()
+
+        self.todos = todos Labirinto( n= SALA1, s= SALA2, l= SALA3, o= SALA4)
+
    
 OI()
