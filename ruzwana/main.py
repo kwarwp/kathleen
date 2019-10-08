1a0104cf3c6b926283fe8ac958d48c391e45db85
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 10:27:13 -0300

    Saved ruzwana/main.py @2019-10-08 13:27

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -1,25 +1,27 @@
 # kathleen.ruzwana.main.py
-from _spy.vittolino.main import Cena, Elemento , Labirinto
+from _spy.vittolino.main import Cena, Elemento, Labirinto, Texto
 from _spy.vittolino.main import inventario as inv
-DOENTE=""
-AJUDANTE=""
-CIENTISTA=""
-LADRAO=""
+DOENTE="https://png.pngtree.com/element_pic/17/09/15/c76996e051a875cb585bf5350d53eabe.jpg"
+AJUDANTE="https://i.pinimg.com/originals/25/48/27/254827fb621ed6c9e50b401d92554810.png"
+CIENTISTA="https://png.pngtree.com/png-clipart/20190604/original/pngtree-the-scientist-png-image_949187.jpg"
+LADRAO="https://cdn.pixabay.com/photo/2017/01/30/21/48/burglar-2022159_960_720.png"
 CAES=""
-BIBLIOTECA=""
-CASA=""
+BIBLIOTECA="https://live.staticflickr.com/30/65319105_09adc58261_z.jpg"
+CASA="https://www.atom.org.br/wp-content/uploads/2018/07/casas-png-casa-png-499-371-499-1.png"
 COMPUTADOR=""
 REMEDIO=""
-PORTA=""
+PORTA="https://cdn.leroymerlin.com.br/products/porta_montada_de_giro_sarrafeada_de_madeira_2,13x0,75m_concrem_wood_89822306_0001_300x300.jpg"
 GUARDAS=""
-LIVRO=""
+LIVRO="https://static3.tcdn.com.br/img/img_prod/280297/2276_1_20170803132328.jpg"
 LABORATORIO=""
-PREDIO=""
-TUNEL=""
+TUNEL="https://www.dm.com.br/wp-content/uploads/2018/08/Sisenando-Francisco-de-Azevedo-2.jpg"
 SAIDA=""
 LIXO=""
-MAPA=""
- class leni():
+MAPA="https://png.pngtree.com/element_origin_min_pic/17/08/13/0637aa84ab59de81fc26e62aa117f071.jpg"
+BILHETE=""
+PORTAO=""
+PRACA=""
+    class leni():
     doente= Elemento(img=DOENTE)
     ajudante= Elemento(img=AJUDANTE)
     cientista= Elemento(img=CIENTISTA)
@@ -32,15 +34,26 @@ MAPA=""
     lixo= Elemento(img=LIXO)
     porta= Elemento(img=PORTA)
     livro= Elemento(img=LIVRO)
+    bilhete= Elemento(img=BILHETE)
+    portao= Elemento(img=PORTAO)
     biblioteca= Cena(img=BIBLIOTECA)
     casa= Cena (img=CASA)
     laboratorio= Cena(img=LABORATORIO)
-    predio= Cena(img=PREDIO)
     tunel= Cena(img=TUNEL)
     saida= Cena(img=SAIDA)
-      doente.entra(casa)
+    pracinha= Cena(img=PRACA)
+    haha= Labirinto(N=casa,S=biblioteca,O=laboratorio,L=porta)
+    hehe= Labirinto(N=porta,S=tunel,O=saida,L=pracinha)
+        doente.entra(casa)
         ajudante.entra(casa)
         ladrao.entra(casa)
         ajudante.entra(biblioteca)
         doente.entra(biblioteca)
+        doente.entra(laboratorio)
+        ladrao.entra(porta)
+        doente.entra(porta)
+        ladrao.entra(tunel)
+        doente.entra(tunel)
+        
+        
         
