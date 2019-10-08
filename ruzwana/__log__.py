e220c5a9de7024bdb75a633948732a2bbfde6e66
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 11:05:56 -0300

    Created ruzwana/__log__.py @2019-10-08 14:05

---
+++ ruzwana/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Oct 08 2019 11:05:54.65 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 24
+  class leni():
+  ^
+IndentationError: unexpected indent
+'''},
