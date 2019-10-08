f44dd48b78b52d6ecb1ca1e797f29088f667fb99
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 13:05:17 -0300

    Saved parisa/main.py @2019-10-08 16:05

--- parisa/main.py
+++ parisa/main.py
@@ -1,13 +1,12 @@
 # kathleen.parisa.main.py
-from _spy.vitollino.main import Cena, Elemento, Labirinto, Texto
+from _spy.vitollino.main import Cena, Elemento, Labirinto, Texto, Codigo
 from _spy.vittolino.main import INVENTARIO as inv
 INICIO = "https://www.loskatchorros.com.br/forum/uploads/monthly_2017_08/maxresdefault.jpg.3845d9d72470fe4e52aa779cf96871b5.jpg"
 
 class OI():
     inicio = Cena(img = INICIO)
-    MENSAGENS = [
-          [ inicio, "Olá"]
-          ]
+    mensagens=Codigo(cena=inicio, topo="olá", codigo="lorem ipsum sic dolor") 
     inicio.vai()
-    MENSAGENS.vai()
+    mensagens.vai()
+    
 OI()
