765caa324740bbd46751ccf30daf2f2f50ed62f6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 09:43:26 -0300

    Saved kristen/__log__.py @2019-10-08 12:43

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -150,3 +150,21 @@ NameError: name 'CARTA' is not defined
     CARTA()
 NameError: name 'CARTA' is not defined
 '''},
+{'date': 'Tue Oct 08 2019 09:43:10.527 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 15
+    class CARTA():
+  module <module> line 42
+    CARTA()
+NameError: name 'CARTA' is not defined
+'''},
