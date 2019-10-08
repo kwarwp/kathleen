5f2d80c94a7f5544ca7cbbb8cd1c3f785e6a08df
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 07:31:04 -0300

    Saved callie/__log__.py @2019-10-08 10:31

--- callie/__log__.py
+++ callie/__log__.py
@@ -80,3 +80,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Tue Oct 08 2019 07:31:03.235 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 18
+  homen=Elemento (img=HOMEM)
+  ^
+IndentationError: expected an indented block
+'''},
