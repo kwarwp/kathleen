7745f75d9b6b63f8a3eaaa5286711cf1a80b0d7f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 13:53:58 -0300

    Saved texto/main.py @2019-10-08 16:53

--- texto/main.py
+++ texto/main.py
@@ -1,11 +1,16 @@
 # kathleen.texto.main.py
 from _spy.vitollino.main import Texto
+from browser import html, document
 
 
 class TextOpt(Texto):
     def __init__(self, cena=NADA, tit="", txt="", foi=None, **kwargs):
         super().__init__(None, tit=tit, txt=txt, vai=None, **kwargs)
         self.optou = None
+        try:
+            self.div = document["__text_opt_div__"]
+        except Exception:
+            self.div = html.DIV(Id="__text_opt_div__")
         self.monta_optar(**kwargs)
     
 
@@ -13,7 +18,7 @@ class TextOpt(Texto):
         def opcao(letra):
             self.optou = letra
 
-            def opta(letra, texto):
+        def opta(letra, texto):
                 div = html.DIV(Class="content")
                 optou = html.A(chr(ABOXED + ord(letra) - ord("A")), Class="option", href="#")
                 optou.onclick = lambda *_: opcao(letra) or self._close()
@@ -22,6 +27,6 @@ class TextOpt(Texto):
                 div <= texto_opcao
                 return div
 
-            optar = [[optou, texto] for optou, texto in kwargs.items() if optou in "ABCDEFGHIJK"]
-            for op in optar:
+        optar = [[optou, texto] for optou, texto in kwargs.items() if optou in "ABCDEFGHIJK"]
+        for op in optar:
                 self.div <= opta(*op)
