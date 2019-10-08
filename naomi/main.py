75ebbf081490aa6004adfd89f173531deb3f42e3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 10:09:12 -0300

    Saved naomi/main.py @2019-10-08 13:09

--- naomi/main.py
+++ naomi/main.py
@@ -6,6 +6,11 @@ CENA = "https://img.freepik.com/fotos-gratis/praia-e-mar_74190-2371.jpg?size=626
 BONECA = "https://static3.tcdn.com.br/img/img_prod/460977/pre_venda_boneco_homem_de_ferro_iron_man_mark_lxxxv_vingadores_ultimato_avengers_endgame_mms528_d30__42292_1_20190402112855.png"
 HOSPITAL= "https://images.vexels.com/media/users/3/144204/isolated/preview/f6d082b22c3fbdc5d2927ff1c7cd57d4---cone-do-edif--cio-do-hospital-by-vexels.png"
 CEU= "https://png.pngtree.com/element_origin_min_pic/16/06/19/015765842a90594.jpg"
+SALA1="https://upload.wikimedia.org/wikipedia/commons/0/0d/Corredor_da_Faculdade_de_Informa%C3%A7%C3%A3o_e_Comunica%C3%A7%C3%A3o%2C_UFG_Samambaia%2C_janeiro_de_2017.jpg"
+SALA2= "https://cdn.pixabay.com/photo/2017/04/07/19/16/infinity-blue-2211659_960_720.jpg"
+SALA3="https://upload.wikimedia.org/wikipedia/commons/8/82/Rodovi%C3%A1ria_de_Curvelo.jpg"
+SALA4= "https://static.escolakids.uol.com.br/conteudo_legenda/74213602df7655dcb813515dbbb1c837.jpg"
+
 class OI():
 		#def _init_main (self)
 		"""cena = Cena ( img = CENA )
@@ -15,7 +20,7 @@ class OI():
 		cena.vai()"""
            
 
-cpu= Labirinto (N=Sala1, S= Sala2, O= Sala3, L= Sala4)
+ceu= Labirinto (N=Sala1, S= Sala2, O= Sala3, L= Sala4)
 boneca.entra(Oeste.Sala3)
 cena.entra(Norte.Sala1)
 hospital.entra(Leste.Sala4)
