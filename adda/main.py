b565ac345456cd92b9a12d56c5024f5b64c5c362
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 11:59:13 -0300

    Saved adda/main.py @2019-10-08 14:59

--- adda/main.py
+++ adda/main.py
@@ -5,19 +5,32 @@ AYDA = "img da ayda"
 NPC_KAYLA = "img da npc"
 SALA1_LAB = "sala do oswaldin com o computador"
 COMPUTADOR_SALA1 = "computador desafio.elemento"
+COMPUTADOR_ELE1 = "tela computador"
+COMPUTADOR_ELE2 = "tela comp. 2"
 SALA2_COFRE = "sala onde vai estar o cofre"
 COFRE_SALA2 = "foto elemento"
 class Tordo():
-    oswaldin = Elemento(img=)
-    ayda = Elemento(img=)
-    kayla = Elemento(img=)
-    sala1 = Cena(img=)
-    computador = Elemento(img=)
-    sala2 = Cena(img=)
-    cofre = Elemento(img=)
+    def __init__(self):
+        self.oswaldin = Elemento(img=)
+        self.ayda = Elemento(img=)
+        self.kayla = Elemento(img=)
+        self.sala1 = Cena(img=)
+        self.computador = Elemento(img=)
+        self.compcena = Cena(img=)
+        self.sala2 = Cena(img=)
+        self.cofre = Elemento(img=)
+
+        self.kayla.entra(sala1)
+        self.falakayla = Texto (sala1,"Olá, aqui é Kayla, inteligência criada pelo Dr. Oswaldinho. Ele está desaparecido e tem uma gangue de cientistas loucos querendo roubar sua fórmula recém criada, você preisa encontrar antes deles!")
+        self.ayda.entra(sala1)
+        self.computador.vai = self.compcena.vai
+        MENSAGENS = [
+                  [ compcena, "Síntese de proteína ?" "Na síntese de proteína ocorre a tradução do código genético e a formação de proteínas"
+                  [ compcena1, "
+                  
+        
+        
+        
     
-    kayla.entra(sala1)
-    falakayla = Texto (sala1,"Olá, aqui é Kayla, inteligência criada pelo Dr. Oswaldinho. Ele está desaparecido e tem uma gangue de cientistas loucos querendo roubar sua fórmula recém criada, você preisa encontrar antes deles!")
-    ayda.entra(sala1)
     
         
