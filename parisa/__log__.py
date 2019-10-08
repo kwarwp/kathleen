4d826847ede755b9735dd26655fcefe318dd9d2a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 13:29:34 -0300

    Saved parisa/__log__.py @2019-10-08 16:29

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -153,3 +153,21 @@ NameError: name 'SALA2' is not defined
     todos = Labirinto( n= SALA1, s= SALA2, o = SALA3, l= SALA4)
 NameError: name 'SALA2' is not defined
 '''},
+{'date': 'Tue Oct 08 2019 10:29:50.576 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 6
+    class OI():
+  module <module> line 7
+    sala1 = Cena(img = INICIO)
+NameError: name 'INICIO' is not defined
+'''},
