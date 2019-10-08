0baeeb827592aeecb3e5286e7565bc6934d8e958
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 07:29:22 -0300

    Saved callie/main.py @2019-10-08 10:29

--- callie/main.py
+++ callie/main.py
@@ -9,3 +9,26 @@ CIENTISTA="https://png.pngtree.com/png-clipart/20190604/original/pngtree-the-sci
 BIBLIOTECA="https://live.staticflickr.com/30/65319105_09adc58261_z.jpg"
 PORTA="https://cdn.leroymerlin.com.br/products/porta_montada_de_giro_sarrafeada_de_madeira_2,13x0,75m_concrem_wood_89822306_0001_300x300.jpg"
 LIVRO="https://static3.tcdn.com.br/img/img_prod/280297/2276_1_20170803132328.jpg"
+PORTA2="https://images.tcdn.com.br/img/img_prod/611678/porta_lisa_semi_solida_angelim_52_1_20190517171407.jpg"
+TUNEL="https://www.dm.com.br/wp-content/uploads/2018/08/Sisenando-Francisco-de-Azevedo-2.jpg"
+MAPA="https://png.pngtree.com/element_origin_min_pic/17/08/13/0637aa84ab59de81fc26e62aa117f071.jpg"
+
+class OVO():
+    # def _init_(self):
+    homem= Elemento (img=HOMEM)
+    casa= Cena (img=CASA)
+    ajudante= Elemento (img=AJUDANTE)
+    ladrao= Elemento (img=LADRAO)
+    cientista= Elemento (img=CIENTISTA)
+    biblioteca= Cena (img=BIBLIOTECA)
+    porta= Cena (img=PORTA)
+    livro= Elemento (img=LIVRO)
+    portinha= Cena (img=PORTA2)
+    tunel= Cena (img=TUNEL)
+    mapinha= Elemento (img=MAPA)
+    homem.entra(casa)
+    casa.direita=biblioteca
+    
+    
+    
+    OVO()
