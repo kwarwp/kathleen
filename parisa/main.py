775f9bd9b8c1b3b1de80c2e41847f0a10ee95470
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 12:49:43 -0300

    Saved parisa/main.py @2019-10-08 15:49

--- parisa/main.py
+++ parisa/main.py
@@ -4,9 +4,9 @@ from _spy.vittolino.main import INVENTARIO as inv
 INICIO = "https://www.loskatchorros.com.br/forum/uploads/monthly_2017_08/maxresdefault.jpg.3845d9d72470fe4e52aa779cf96871b5.jpg"
 
 class OI():
-inicio = Cena(img = INICIO)
-MENSAGENS = [
+    inicio = Cena(img = INICIO)
+    MENSAGENS = [
           [ inicio, "Olá"]
           ]
-      inicio.vai()
+    inicio.vai()
 OI()
