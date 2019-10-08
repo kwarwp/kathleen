01cfb26fce44e336d066845ef814d87726fbc9a1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 13:31:21 -0300

    Saved rachel/main.py @2019-10-08 16:31

--- rachel/main.py
+++ rachel/main.py
@@ -3,3 +3,4 @@ from _spy.vitollino.main import Cena, Texto, Elemento, STYL
 from _spy.vitollino.main import INVENTARIO as inv
 
 laboratorio = http://coveli.com.br/wp-content/uploads/2017/09/img-laboratorio.png
+cherles = 
