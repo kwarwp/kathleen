b46588a04ea0c7c0d34e1f6d8cc5a38f52a537b8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 10:47:37 -0300

    Saved adda/main.py @2019-10-08 13:47

--- adda/main.py
+++ adda/main.py
@@ -1,1 +1,18 @@
 # kathleen.adda.main.py 
+from _spy.vittolino.main import  Cena, Elemento, Labirinto, Texto
+from _spy.vittolino.main import Inventario as inv
+AYDA = "img da ayda"
+NPC_KAYLA = "img da npc"
+SALA1_LAB = "sala do oswaldin com o computador"
+COMPUTADOR_SALA1 = "computador desafio.elemento"
+SALA2_COFRE = "sala onde vai estar o cofre"
+COFRE_SALA2 = "foto elemento"
+class Tordo():
+     oswaldin = Elemento(img=)
+    ayda = Elemento(img=)
+    kayla = Elemento(img=)
+    sala1 = Cena(img=)
+    computador = Elemento(img=)
+    sala2 = Cena(img=)
+    cofre = Elemento(img=)
+     kayla.entra(sala1)
