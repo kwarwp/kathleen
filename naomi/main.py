311873e0f5f0d9d8ef42e164adda1f5750fe4788
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 10:01:43 -0300

    Saved naomi/main.py @2019-10-08 13:01

--- naomi/main.py
+++ naomi/main.py
@@ -1,27 +1,26 @@
 # kathleen.naomi.main.py
-from _spy.vitollino.main import Cena, Elemento 
+from _spy.vitollino.main import Cena, Elemento, Labirinto 
 from _spy.vitollino.main import INVENTARIO as inv
 
 CENA = "https://img.freepik.com/fotos-gratis/praia-e-mar_74190-2371.jpg?size=626&ext=jpg"
 BONECA = "https://static3.tcdn.com.br/img/img_prod/460977/pre_venda_boneco_homem_de_ferro_iron_man_mark_lxxxv_vingadores_ultimato_avengers_endgame_mms528_d30__42292_1_20190402112855.png"
+HOSPITAL= "https://images.vexels.com/media/users/3/144204/isolated/preview/f6d082b22c3fbdc5d2927ff1c7cd57d4---cone-do-edif--cio-do-hospital-by-vexels.png"
+CEU= "https://png.pngtree.com/element_origin_min_pic/16/06/19/015765842a90594.jpg"
 class OI():
 		#def _init_main (self)
-		cena = Cena ( img = CENA )
+		"""cena = Cena ( img = CENA )
 		boneca = Elemento ( img = BONECA, style=dict(
             left=100, top=150, width=200, height="150px"))  
 		boneca.entra(cena)
-		cena.vai()
+		cena.vai()"""
            
-         
-         
-OI() 
-GABINETE = "https://i.imgur.com/dp2Gnak.png"
-DELEGACIA = "https://image.flaticon.com/icons/png/512/551/551512.png"
-FESTA = "https://img.freepik.com/vetores-gratis/silhueta-de-um-publico-de-festa_1048-9714.jpg?size=626&ext=jpg"
-CEMITERIO = "https://images.vexels.com/media/users/3/130673/isolated/preview/c58973769fe0badffc8ec348fe555f20---cone-do-cemit--rio-by-vexels.png"
-DELEGADO = "https://images.vexels.com/media/users/3/158713/isolated/preview/7ad4403a507c6cf10d9d34093f6fa690-ilustra----o-uniforme-de-policial-by-vexels.png"
-MORDOMO = "http://clipart.coolclips.com/480/vectors/tf05165/CoolClips_vc009999.png"
-POLITICO = "https://pngimage.net/wp-content/uploads/2018/06/politico-png-6.png"
-HOMEM = "https://thumbs.dreamstime.com/t/homem-negro-novo-que-sorri-contra-o-fundo-branco-96739013.jpg"
-PASSISTA = "https://images.vexels.com/media/users/3/151285/isolated/preview/43ea02778621af6aca3631f2ba619bf5-mulher-carnaval-dan--ar-by-vexels.png"
+
+cpu= Labirinto (N=Sala1, S= Sala2, O= Sala3, L= Sala4)
+boneca.entra(Oeste.Sala3)
+cena.entra(Norte.Sala1)
+hospital.entra(Leste.Sala4)
+ceu.vai()
+
+OI()
+
 
