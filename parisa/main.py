174399ffd8711fa6f3a75c43b3c97ea223de6ec3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 12:48:36 -0300

    Saved parisa/main.py @2019-10-08 15:48

--- parisa/main.py
+++ parisa/main.py
@@ -1,1 +1,12 @@
 # kathleen.parisa.main.py
+from _spy.vitollino.main import Cena, Elemento, Labirinto, Texto
+from _spy.vittolino.main import INVENTARIO as inv
+INICIO = "https://www.loskatchorros.com.br/forum/uploads/monthly_2017_08/maxresdefault.jpg.3845d9d72470fe4e52aa779cf96871b5.jpg"
+
+class OI():
+inicio = Cena(img = INICIO)
+MENSAGENS = [
+          [ inicio, "Olá"]
+          ]
+      inicio.vai()
+OI()
