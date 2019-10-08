1c2c8f2bcedad517ad19f24522e9415ce31506dd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 13:00:32 -0300

    Saved ruzwana/__log__.py @2019-10-08 16:00

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -41,3 +41,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Tue Oct 08 2019 13:00:30.990 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 24
+  class LENI():
+  ^
+IndentationError: unexpected indent
+'''},
