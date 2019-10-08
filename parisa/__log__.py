734f8c4c7773a324cedb25bce739e6e37a444bec
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 12:55:09 -0300

    Saved parisa/__log__.py @2019-10-08 15:55

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -31,3 +31,25 @@ IndentationError: expected an indented block
     MENSAGENS.vai()
 AttributeError: 'list' object has no attribute 'vai'
 '''},
+{'date': 'Tue Oct 08 2019 12:55:08.939 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 6
+    class OI():
+  module <module> line 12
+    MENSAGENS.vai()
+AttributeError: 'list' object has no attribute 'vai'
+'''},
