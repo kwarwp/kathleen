989c23620624c45358f35a87d0d435e147a73190
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 10:13:20 -0300

    Saved naomi/__log__.py @2019-10-08 13:13

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -370,3 +370,19 @@ NameError: name 'Sala1' is not defined
     ceu= Labirinto (N=Sala1, S= Sala2, O= Sala3, L= Sala4)
 NameError: name 'Sala1' is not defined
 '''},
+{'date': 'Tue Oct 08 2019 10:13:17.473 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 23
+    ceu= Labirinto (N=SALA1, S= SALA2, O= SALA3, L= SALA4)
+TypeError: __init__() got an unexpected keyword argument 'N'
+'''},
