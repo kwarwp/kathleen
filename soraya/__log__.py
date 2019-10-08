e3830e9ca949f3881aa257080996d2e72f22ccf1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 11:01:23 -0300

    Saved soraya/__log__.py @2019-10-08 14:01

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -104,3 +104,17 @@ KeyError: burg_menu
     do_menu(self.doc['burg_menu'])
 KeyError: burg_menu
 '''},
+{'date': 'Tue Oct 08 2019 11:01:22.44 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.main line 218
+    do_menu(self.doc['burg_menu'])
+KeyError: burg_menu
+'''},
