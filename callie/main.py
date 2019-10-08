53e82ee31f581c0242fd364771a4d5a6307cab4e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-08 06:01:49 -0300

    Created callie/main.py @2019-10-08 09:01

---
+++ callie/main.py
@@ -0,0 +1,10 @@
+# kathleen.callie.main.py
+from _spy.vittolino.main import Cena, Elemento, Labirinto
+from _spy.vittolino.main import INVENTARIO as inv
+HOMEM="https://png.pngtree.com/element_pic/17/09/15/c76996e051a875cb585bf5350d53eabe.jpg"
+AJUNDANTE="https://i.pinimg.com/originals/25/48/27/254827fb621ed6c9e50b401d92554810.png"
+LADRAO="https://cdn.pixabay.com/photo/2017/01/30/21/48/burglar-2022159_960_720.png"
+CASA="https://www.atom.org.br/wp-content/uploads/2018/07/casas-png-casa-png-499-371-499-1.png"
+CIENTISTA="https://png.pngtree.com/png-clipart/20190604/original/pngtree-the-scientist-png-image_949187.jpg"
+BIBLIOTECA="https://live.staticflickr.com/30/65319105_09adc58261_z.jpg"
+PORTA="https://cdn.leroymerlin.com.br/products/porta_montada_de_giro_sarrafeada_de_madeira_2,13x0,75m_concrem_wood_89822306_0001_300x300.jpg"
