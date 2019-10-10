1d3c6e84ff28ae619661615c070330ed4d26fe4c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-10 11:46:53 -0300

    Saved parisa/main.py @2019-10-10 14:46

--- parisa/main.py
+++ parisa/main.py
@@ -25,7 +25,7 @@ class OI():
         falalogo=Texto(todos.norte,"O intuito do jogo é fazer com que as pessoas tenham ciência de que a prática do bullying pode causar mal à saúde humana. Resultando em aspectos que possam dificultar o cotidiano de quem é alvo dessa barbárie, e, saber o limite de brincadeira e agressão. ")
         logo.vai=falalogo.vai
         
-        neide=Elemento(img=NEIDE)
+        neide=Elemento(img=NEIDE, style=dict(left=370, top=200, width=450, height="250px"))
         neide.entra(todos.leste)
         
         
