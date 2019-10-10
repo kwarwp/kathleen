82fdf73e34dc021f09be171999b5f1e538a0b3d9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-10 10:47:14 -0300

    Saved parisa/__log__.py @2019-10-10 13:47

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -349,3 +349,10 @@ SyntaxError: non-keyword arg after keyword arg
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Thu Oct 10 2019 10:47:13.768 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 21
+  self. = Texto (sala1,"Olá, aqui é Kayla")
+  ^
+IndentationError: unexpected indent
+'''},
