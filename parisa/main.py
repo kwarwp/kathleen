6157c8b6947f59f75e18da0f24b72c0a9648d36f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-10 10:41:42 -0300

    Saved parisa/main.py @2019-10-10 13:41

--- parisa/main.py
+++ parisa/main.py
@@ -9,13 +9,14 @@ SALA2 = "http://www.colegiohoje.com.br/img/predioEscola.png"
 SALA3 = "https://estaticos.globoradio.globo.com/fotos/2018/05/6eb89bac-521f-4bac-b3a9-8a664831f478.jpg.640x360_q75_box-0%2C49%2C640%2C408_crop_detail.jpg"
 #Corredor da escola CENA 4
 SALA4 = "https://cdn.pixabay.com/photo/2014/08/13/20/16/school-417612_960_720.jpg"
+#Quadra de esportes
 class OI():
     def __init__(self):
         self.todos = todos = Sala( n= SALA1, s= SALA3, l= SALA2, o= SALA4)
         todos.norte.vai()
-        mensagens= Codigo(cena=todos.norte, topo="", codigo="O intuito do jogo é fazer com que as pessoas" 
-        "tenham ciência de que a , prática do bullying pode causar mal à saúde humana."
-        "Resultando em aspectos que possam dificultar o cotidiano de quem é alvo dessa barbárie, e, saber o limite de brincadeira e agressão.") 
+        mensagens= Codigo(cena=todos.norte, topo="", codigo="O intuito do jogo é fazer com que as pessoas") 
+        ("tenham ciência de que a , prática do bullying pode causar mal à saúde humana.")
+        ("Resultando em aspectos que possam dificultar o cotidiano de quem é alvo dessa barbárie, e, saber o limite de brincadeira e agressão.") 
         mensagens.vai()
 
    
