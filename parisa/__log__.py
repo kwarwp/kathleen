d9d1c21bb3cb41c775ac3cfe9ef7116e37992faf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-17 15:29:21 -0300

    Saved parisa/__log__.py @2019-10-17 18:29

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -832,3 +832,21 @@ NameError: name 'SALA6' is not defined
     self.todos = todos = Sala( o= SALA4, s=SALA5, l=SALA6, n=SALA1)
 NameError: name 'SALA6' is not defined
 '''},
+{'date': 'Thu Oct 17 2019 08:29:27.894 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 48
+    OI()
+  module <module> line 23
+    self.todos = todos = Sala( o= SALA4, s=SALA5, l=SALA6, n=SALA1)
+NameError: name 'SALA6' is not defined
+'''},
