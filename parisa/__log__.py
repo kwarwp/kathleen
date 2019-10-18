15d2a8e5408a8a2d14b40b63f67dd97dc4d79b91
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-18 17:40:21 -0300

    Saved parisa/__log__.py @2019-10-18 20:40

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -1224,3 +1224,21 @@ NameError: name 'entra' is not defined
     logo.entra(entra.norte)
 NameError: name 'entra' is not defined
 '''},
+{'date': 'Fri Oct 18 2019 17:40:19.275 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 57
+    OI()
+  module <module> line 28
+    logo.entra(entra.norte)
+NameError: name 'entra' is not defined
+'''},
