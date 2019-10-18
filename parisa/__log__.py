ae527e4e6c285a732fce8cf5779094c5f238d11b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-18 16:34:33 -0300

    Saved parisa/__log__.py @2019-10-18 19:34

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -1116,3 +1116,21 @@ AttributeError: 'str' object has no attribute 'norte'
     logo.entra(SALA1.norte)
 AttributeError: 'str' object has no attribute 'norte'
 '''},
+{'date': 'Fri Oct 18 2019 16:34:32.500 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 57
+    OI()
+  module <module> line 28
+    logo.entra(entra.sala1.norte)
+NameError: name 'entra' is not defined
+'''},
