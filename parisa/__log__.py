62c3e68d424e1cd0cc3e38142df36bb49af359ba
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-18 16:31:32 -0300

    Saved parisa/__log__.py @2019-10-18 19:31

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -1044,3 +1044,21 @@ NameError: name 'SALA6' is not defined
     logo.entra(todos.SALA1)
 AttributeError: 'Sala' object has no attribute 'SALA1'
 '''},
+{'date': 'Fri Oct 18 2019 16:31:31.490 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 57
+    OI()
+  module <module> line 28
+    logo.entra(SALA1.norte)
+AttributeError: 'str' object has no attribute 'norte'
+'''},
