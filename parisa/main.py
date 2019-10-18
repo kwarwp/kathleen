0f1ef6ba39388009aa7e3c78a8e049161e653153
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-10-18 16:33:08 -0300

    Saved parisa/main.py @2019-10-18 19:33

--- parisa/main.py
+++ parisa/main.py
@@ -23,7 +23,7 @@ class OI():
         self.todos = todos = Sala( n= SALA1, s= SALA3, l= SALA2, o= SALA4)
         todos.norte.vai()
         self.todos = todos = Sala( o= SALA4, s=SALA5, l=SALA6, n=SALA1)
-        #todos.norte.vai()
+        todos.norte.vai()
         logo = Elemento(img=LOGO, style=dict(left=370, top=200, width=450, height="250px"))
         logo.entra(SALA1.norte)
         falalogo=Texto(SALA1.norte,"O intuito do jogo é fazer com que as pessoas tenham ciência de que a prática do bullying pode causar mal à saúde humana. Resultando em aspectos que possam dificultar o cotidiano de quem é alvo dessa barbárie, e, saber o limite de brincadeira e agressão. ")
