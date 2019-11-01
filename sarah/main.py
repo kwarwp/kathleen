2abd56ff201b8f64f3216c1afea431fceecbe270
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 09:39:19 -0300

    Saved sarah/main.py @2019-11-01 12:39

--- sarah/main.py
+++ sarah/main.py
@@ -1,7 +1,7 @@
 # kathleen.sarah.main.py
 from _spy vitollino.main import Cena, Elemento
 from _spy vitollino.main import INVENTARIO as inv
-PROFESSOR ="https://images.vexels.com/media/users/3/128130/isolated/preview/686ab100dddd65fc0ea71e7bb4c3db6f-senhora-professora-dos-desenhos-animados-by-vexels.png"
+PROFESSOR ="https://upload.wikimedia.org/wikipedia/commons/b/b1/Professor.png"
 ALUNO="https://myrealdomain.com/images/alunos-png.png"
 SALA="https://cdn.pixabay.com/photo/2018/09/30/11/55/white-board-3713307_960_720.png"
 ESCOLA="http://www.kademi.com.br/images/external_layout/escola.png"
