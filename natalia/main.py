4832b9c91e1b1c2bdd0f62aff45f73c916fbd4f1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 09:06:53 -0300

    Saved natalia/main.py @2019-11-01 12:06

--- natalia/main.py
+++ natalia/main.py
@@ -1,1 +1,17 @@
 # kathleen.natalia.main.
+from _spy.vittolino.main import Cena, Elemento
+from _spy.vittolino.main import Inventario
+ESCOLA = "https://images.educamaisbrasil.com.br/content/basico/instituicao/logo/g/colegio-ndi.png"
+SALA = "http://www.adel.org.br/wp-content/uploads/2016/07/Sala-de-Aula.png"
+ALUNO = "http://www.ifmg.edu.br/ceadop3/imagens/aluno.png/@@images/image.png"
+PROFESSOR = "https://upload.wikimedia.org/wikipedia/commons/b/b1/Professor.png"
+	class PIBIC ():
+         escola = Cena(img = ESCOLA)
+         sala = Cena(img = SALA)
+         professor = Elemento(img= PROFESSOR)
+         aluno = Elemento(img = ALUNO)
+         professor.entra(sala)
+         aluno.entra(escola)
+         sala.vai
+         escola.vai
+      PIBIC()   
