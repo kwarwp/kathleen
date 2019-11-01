47acc75290066608365e79e4e19684b2c93916b1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 09:35:58 -0300

    Saved sarah/main.py @2019-11-01 12:35

--- sarah/main.py
+++ sarah/main.py
@@ -6,8 +6,8 @@ ALUNO="https://myrealdomain.com/images/alunos-png.png"
 SALA="https://cdn.pixabay.com/photo/2018/09/30/11/55/white-board-3713307_960_720.png"
 ESCOLA="http://www.kademi.com.br/images/external_layout/escola.png"
 class pibic():
-    professor=Elemento(img=PROFESSOR)
-    aluno=Elemento(img=ALUNO)
+    professor= Elemento(img=PROFESSOR)
+    aluno= Elemento(img=ALUNO)
     sala=Cena(img=SALA)
     escola=Cena(img=ESCOLA)
     aluno.entra(escola)
