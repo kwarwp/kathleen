61f9ba6bae790f2cf37277bbbc89a99637b23793
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 11:06:10 -0300

    Saved sarah/main.py @2019-11-01 14:06

--- sarah/main.py
+++ sarah/main.py
@@ -4,3 +4,8 @@ from _spy.vitollino.main import INVENTARIO as inv
 PISTA="https://cdn.aquelamaquina.pt/images/2018-10/img_944x629$2018_10_23_13_20_29_124325.png"
 CARRO="https://www.pnglot.com/pngfile/detail/143-1438797_carro-png-imagenes-de-carros-png.png"
 class yuri():
+    pista=Cena(img=PISTA)
+    carro=Elemento(img=CARRO)
+    carro.entra(pista)
+    pista.vai()
+yuri()    
