75a2eaa17af8b430a6503c9fbabec3d7ce44dcaf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 09:19:18 -0300

    Saved natalia/main.py @2019-11-01 12:19

--- natalia/main.py
+++ natalia/main.py
@@ -7,9 +7,9 @@ ALUNO = "http://www.ifmg.edu.br/ceadop3/imagens/aluno.png/@@images/image.png"
 PROFESSOR = "https://upload.wikimedia.org/wikipedia/commons/b/b1/Professor.png"
 class pibic():
     escola=Cena(img=ESCOLA)
-    sala = Cena(img = SALA)
-    professor = Elemento(img= PROFESSOR)
-    aluno = Elemento(img = ALUNO)
+    sala = Cena(img=SALA)
+    professor= Elemento(img=PROFESSOR)
+    aluno=Elemento(img=ALUNO)
     professor.entra(sala)
     aluno.entra(escola)
     sala.vai
