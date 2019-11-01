51e9f49e2cb8918fa7b7a3c798e884db58430f10
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 09:07:06 -0300

    Saved natalia/__log__.py @2019-11-01 12:07

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -409,3 +409,10 @@ Traceback (most recent call last):
   module <module> line 2
     from _spy.vitollino.main import Cena, Elemnento
 '''},
+{'date': 'Fri Nov 01 2019 09:05:52.824 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  class PIBIC ():
+  ^
+IndentationError: unexpected indent
+'''},
