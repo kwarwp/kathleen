800770bf899a86563bb636cf3deb0a4d706e7d69
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-07 14:42:37 -0300

    Created courtney/main.py @2019-11-07 17:42

---
+++ courtney/main.py
@@ -0,0 +1,5 @@
+# kathleen.courtney.main.py
+from _spy.vitollino.main import Cena,Elemento
+from _spy.vitollino.main import INVENTARIO as inv
+ESTUDANTE = "https://img.lovepik.com/element/40056/5743.png_860.png
+ESCOLA = "https://opengameart.org/sites/default/files/school_0.png
