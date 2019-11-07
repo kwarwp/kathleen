8798eb757680742410a2af80a905031203b69e6d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-07 15:13:36 -0300

    Saved tracy/main.py @2019-11-07 18:13

--- tracy/main.py
+++ tracy/main.py
@@ -5,4 +5,5 @@ JIMIN="https://p1.hiclipart.com/preview/967/349/249/park-jimin-png-clipart.jpg"
 ESCOLA="https://patrilefundes.files.wordpress.com/2015/06/image17.jpg"
 NAMJOON="https://pm1.narvii.com/7211/60d889b892f17262cbbf5e7d57b8b442239d77e5r1-1280-1920v2_hq.jpg"
 CORREDOR="https://comps.canstockphoto.com.br/corredor-escola-vazio-banco-de-fotos_csp1623583.jpg"
-IRI
+IRIS="https://1.bp.blogspot.com/-Ae1vWajOLO8/WGFTyZiQ3tI/AAAAAAAAAOg/HM4w8gi_F_wxhcOLNqhqvHq3WEWPXAQ7QCEw/s1600/1e0db0015ffc222b1f9a637c6265d17b.jpg"
+GINASIO="https://comps.canstockphoto.com.br/iluminado-escola-ch%C3%A3o-luz-gin%C3%A1sio-banco-de-imagens_csp12194809.jpg"
