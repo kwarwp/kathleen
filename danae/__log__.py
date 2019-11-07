a56ba5c488a4bedd7b5f4e0a47afae1d8719aea1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-07 13:49:44 -0300

    Saved danae/__log__.py @2019-11-07 16:49

--- danae/__log__.py
+++ danae/__log__.py
@@ -179,3 +179,25 @@ Traceback (most recent call last):
     setinhas= Elementos(img=SETASFLORESCENTES)
 NameError: name 'Elementos' is not defined
 '''},
+{'date': 'Thu Nov 07 2019 14:50:30.897 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 15
+    class leni():
+  module <module> line 22
+    setinhas= Elementos(img=SETASFLORESCENTES)
+NameError: name 'Elementos' is not defined
+'''},
