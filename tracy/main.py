215d2518abd4ba6c19005890e465d4c40485745c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-07 15:03:08 -0300

    Saved tracy/main.py @2019-11-07 18:03

--- tracy/main.py
+++ tracy/main.py
@@ -1,1 +1,8 @@
 # kathleen.tracy.main.py
+from _spy.vitollino.maim import Cena,Elemento 
+from _spy.vitollino.main import INVENTARIO as inv
+JIMIN="https://p1.hiclipart.com/preview/967/349/249/park-jimin-png-clipart.jpg"
+ESCOLA="https://patrilefundes.files.wordpress.com/2015/06/image17.jpg"
+NAMJOON="https://pm1.narvii.com/7211/60d889b892f17262cbbf5e7d57b8b442239d77e5r1-1280-1920v2_hq.jpg"
+CORREDOR="https://comps.canstockphoto.com.br/corredor-escola-vazio-banco-de-fotos_csp1623583.jpg"
+IRI
