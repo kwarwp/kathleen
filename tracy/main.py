70070cc37a8e9e89ec1973778d081555c278d0de
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-07 15:39:47 -0300

    Saved tracy/main.py @2019-11-07 18:39

--- tracy/main.py
+++ tracy/main.py
@@ -7,3 +7,16 @@ NAMJOON="https://pm1.narvii.com/7211/60d889b892f17262cbbf5e7d57b8b442239d77e5r1-
 CORREDOR="https://comps.canstockphoto.com.br/corredor-escola-vazio-banco-de-fotos_csp1623583.jpg"
 IRIS="https://1.bp.blogspot.com/-Ae1vWajOLO8/WGFTyZiQ3tI/AAAAAAAAAOg/HM4w8gi_F_wxhcOLNqhqvHq3WEWPXAQ7QCEw/s1600/1e0db0015ffc222b1f9a637c6265d17b.jpg"
 GINASIO="https://comps.canstockphoto.com.br/iluminado-escola-ch%C3%A3o-luz-gin%C3%A1sio-banco-de-imagens_csp12194809.jpg"
+class hato()
+    jimin=Elemento(img=JIMIN)
+    ecola=Cena(img=ESCOLA)
+    namjoon=Elemento(img=NAMJOON)
+    corredor=Cena(img=CORREDOR)
+    iris=Elemento(img=IRIS)
+    ginasio=Cena(img=GINASIO)
+    jimin.entra (escola)
+    escola.vai()
+    Namjoon.entra (corredor)
+    corredor.vai()
+    Iris.entra (ginasio)
+    ginasio.vai()
