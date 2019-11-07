cc910f729b3d53827e67373059d0321f727e6e84
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-07 13:26:58 -0300

    Saved sara/__log__.py @2019-11-07 16:26

--- sara/__log__.py
+++ sara/__log__.py
@@ -436,3 +436,25 @@ AttributeError: 'str' object has no attribute 'cenas'
     self.centro.cenas[indice].portal(N=sala.cenas[indice]) if sala else None
 AttributeError: 'str' object has no attribute 'cenas'
 '''},
+{'date': 'Thu Nov 07 2019 14:28:12.565 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 12
+    class CHOCOLATE():
+  module <module> line 20
+    papel= Labirinto(n=SALA1,s=SALA2)
+  module _spy.vitollino.main line 839
+    self.lb()
+  module _spy.vitollino.main line 843
+    self.centro.cenas[indice].portal(N=sala.cenas[indice]) if sala else None
+AttributeError: 'str' object has no attribute 'cenas'
+'''},
