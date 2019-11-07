ce08414b929ef0fed04b5114d5ab629b7aade2cd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-07 14:22:47 -0300

    Saved danae/main.py @2019-11-07 17:22

--- danae/main.py
+++ danae/main.py
@@ -4,14 +4,14 @@ from _spy.vitollino.main import INVENTARIO as inv
 CIENTISTA="https://2.bp.blogspot.com/-380MYApQgg0/T_CU1Iy8f9I/AAAAAAAAFKs/9X5_tDN93YU/s1600/Professor+Hubert+Farnsworth.png"
 AJUDANTE="https://pngimage.net/wp-content/uploads/2018/06/personagens-png-4.png"
 LABORATORIO="https://s03.video.glbimg.com/x720/7727950.jpg"
-JARDIM=""
+JARDIM="https://www.decorfacil.com/wp-content/uploads/2018/05/20180523plantas-para-jardim.jpg"
 CAIXADEMUSICA=""
 HOLOGRAMA=""
 JOGADOR=""
-HOSPITAL=""
+HOSPITAL="https://midiastm.gazetaonline.com.br/_midias/jpg/2019/09/05/fernando_madeira-6299927.jpg"
 ANTIDOTO=""
 PAREDE=""
-SETASFLORESCENTES=""
+SETASFLORESCENTES="https://img1.gratispng.com/20180721/ywy/kisspng-computer-icons-arrow-clip-art-three-arrows-5b52c0d67c4084.5997144015321499745089.jpg"
 class leni():
     cientista= Elemento(img=CIENTISTA)
     ajudante= Elemento(img=AJUDANTE)
@@ -19,7 +19,7 @@ class leni():
     jogador= Elemento(img=JOGADOR)
     holograma= Elemento(img=HOLOGRAMA)
     antidoto= Elemento(img=ANTIDOTO)
-    setinhas= Elementos(img=SETASFLORESCENTES)
+    setinhas= Elemento(img=SETASFLORESCENTES)
     laboratorio= Cena(img=LABORATORIO)
     jardim= Cena(img=JARDIM)
     hospital= Cena(img=HOSPITAL)
