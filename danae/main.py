38b0f28578c7a74e332cf2a859d89389457c8284
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-07 13:46:32 -0300

    Saved danae/main.py @2019-11-07 16:46

--- danae/main.py
+++ danae/main.py
@@ -1,9 +1,9 @@
 # kathleen.danae.main.py
-from _spy.vittolino.main import Cena, Elemento , Labirinto
-from _spy.vittolino.main import inventario as inv
-CIENTISTA=""
-AJUDANTE=""
-LABORATORIO=""
+from _spy.vitollino.main import Cena,Elemento,Texto
+from _spy.vitollino.main import inventario as inv
+CIENTISTA="https://2.bp.blogspot.com/-380MYApQgg0/T_CU1Iy8f9I/AAAAAAAAFKs/9X5_tDN93YU/s1600/Professor+Hubert+Farnsworth.png"
+AJUDANTE="https://pngimage.net/wp-content/uploads/2018/06/personagens-png-4.png"
+LABORATORIO="https://s03.video.glbimg.com/x720/7727950.jpg"
 JARDIM=""
 CAIXADEMUSICA=""
 HOLOGRAMA=""
@@ -12,7 +12,7 @@ HOSPITAL=""
 ANTIDOTO=""
 PAREDE=""
 SETASFLORESCENTES=""
- class CLOCK():
+ class leni():
     cientista= Elemento(img=CIENTISTA)
     ajudante= Elemento(img=AJUDANTE)
     caixinha= Elemento(img=CAIXADEMUSICA)
@@ -24,4 +24,6 @@ SETASFLORESCENTES=""
     jardim= Cena(img=JARDIM)
     hospital= Cena(img=HOSPITAL)
     parede= Cena(img=PAREDE)
+    cientista.entra(laboratorio)
+    ajudante.entra(laboratorio)
     
