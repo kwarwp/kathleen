51a69f5dbf5363acce92f172ebc30fb3e58563da
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-07 13:47:31 -0300

    Created danae/__log__.py @2019-11-07 16:47

---
+++ danae/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Nov 07 2019 14:48:16.619 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 15
+  class leni():
+  ^
+IndentationError: unexpected indent
+'''},
