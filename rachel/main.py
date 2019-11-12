f40764ff074103c6c048b50a00da3893a3b0c4be
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-12 11:09:34 -0300

    Saved rachel/main.py @2019-11-12 14:09

--- rachel/main.py
+++ rachel/main.py
@@ -1,18 +1,18 @@
 # kathleen.rachel.main.py
-from _spy.vittolino.main import Elemento, Cena, Texto
-from _spy.vittolino.main import Inventario as invi 
-MARTE = "https://abrilsuperinteressante.files.wordpress.com/2019/05/site_marte.png"
-MINIOS = "https://imagensemoldes.com.br/wp-content/uploads/2018/05/Meu-Malvado-Favorito-Minions-Jerry-4-PNG.png"
-MORTI = "https://png.pngtree.com/png-clipart/20190611/original/pngtree-vector-fog-png-image_2120607.jpg"
-HUCK = "http://www.pngall.com/wp-content/uploads/2016/03/Avengers-Hulk-PNG.png"
+from _spy.vitollino.main import Elemento, Cena, Texto
+from _spy.vitollino.main import Inventario as invi 
+MARTE="https://abrilsuperinteressante.files.wordpress.com/2019/05/site_marte.png"
+MINIOS="https://imagensemoldes.com.br/wp-content/uploads/2018/05/Meu-Malvado-Favorito-Minions-Jerry-4-PNG.png"
+MORTI="https://png.pngtree.com/png-clipart/20190611/original/pngtree-vector-fog-png-image_2120607.jpg"
+HUCK="http://www.pngall.com/wp-content/uploads/2016/03/Avengers-Hulk-PNG.png"
 class peixe ():
-    arte=Cena(img=MARTE)
+    marte=Cena(img=MARTE)
+    morti=Cena(img=MORTI)
     minios=Elemento(img=MINIOS)
-    minios.entra(arte)
-    arte.direita=mort
-    mort=Cena(img=MORTI)
-    juck=Elemento(img=HUCK)
-    JUCK.entra(mort)
-    mort.esquerda=arte
-    arte.entra()
+    minios.entra(marte)
+    huck=Elemento(img=HUCK)
+    huck.entra(morti)
+    marte.direita=morti
+    morti.esquerda=marte
+    marte.vai()
 peixe()
