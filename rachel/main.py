62b5a7c9e739d0e71c9b4edda0663b57ab56ee29
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-12 11:02:58 -0300

    Saved rachel/main.py @2019-11-12 14:02

--- rachel/main.py
+++ rachel/main.py
@@ -1,6 +1,18 @@
 # kathleen.rachel.main.py
-from _spy.vitollino.main import Cena, Texto, Elemento, STYL
-from _spy.vitollino.main import INVENTARIO as inv
-
-laboratorio = http://coveli.com.br/wp-content/uploads/2017/09/img-laboratorio.png
-cherles = 
+from _spy.vittolino.main import Elemento, Cena, Texto
+from _spy.vittolino.main import Inventario as invi 
+MARTE = "https://abrilsuperinteressante.files.wordpress.com/2019/05/site_marte.png"
+MINIOS = "https://imagensemoldes.com.br/wp-content/uploads/2018/05/Meu-Malvado-Favorito-Minions-Jerry-4-PNG.png"
+MORTI = "https://png.pngtree.com/png-clipart/20190611/original/pngtree-vector-fog-png-image_2120607.jpg"
+HUCK = "http://www.pngall.com/wp-content/uploads/2016/03/Avengers-Hulk-PNG.png"
+class peixe ():
+    arte=Cena(img=MARTE)
+    minios=Elemento(img=MINIOS)
+    minios.entra(arte)
+    arte.direita=mort
+    mort=Cena(img=MORTI)
+    juck=Elemento(img=HUCK)
+    JUCK.entra(mort)
+    mort.esquerda=arte
+    arte.entra()
+peixe()
