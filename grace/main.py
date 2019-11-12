64f56e9a52ab10d08e49bb56e2fc1e80e8caa1c6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-12 10:47:32 -0300

    Saved grace/main.py @2019-11-12 13:47

--- grace/main.py
+++ grace/main.py
@@ -1,16 +1,16 @@
 # kathleen.grace.main.py
-from _spy.vittolino.main import Cena,Elemento,Labirinto e Texto
-from _spy.vittolino.main import INVENTARIO as inv
+from _spy.vitollino.main import Cena,Elemento,Labirinto e Texto
+from _spy.vitollino.main import INVENTARIO as inv
 PIRANHA="http://www.pngmart.com/files/7/Piranha-PNG-Free-Download.png"
 ESPACO="https://img2.gratispng.com/20180420/yie/kisspng-light-andromeda-galaxy-outer-space-sky-cosmos-5ad9df10333e65.6204317315242278562099.jpg" 
 INES="http://3.bp.blogspot.com/-rgWJgYjJV7I/UPtVOtHWsfI/AAAAAAAAAB8/StaiQ21jcTA/s1600/2012-12-13-ines-brasil-bbb-13-babado-e-confusao-querida-6.png"
 IGREJA="https://i.pinimg.com/originals/d6/48/be/d648be937077a693a2fcb0ef50a942ef.png"
 class bruninha ():
-    piranha=Elemento (img=PIRANHA)
-    espaco=Cena (img=ESPACO)
-    ines=Elemento (img=INES)
-    igreja=Cena (img=IGREJA)
+    espaco=Cena(img=ESPACO)
+    igreja=Cena(img=IGREJA)
+    piranha=Elemento(img=PIRANHA)
+    ines=Elemento(img=INES)
     piranha.entra(espaco)
     ines.entra(igreja)
-    espaco.vai()
+    espaco.vai
 bruninha()
