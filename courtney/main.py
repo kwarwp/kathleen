4403a7d8d77f4c9aa015f49a713aa8ed45f15c0e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-12 11:41:18 -0300

    Saved courtney/main.py @2019-11-12 14:41

--- courtney/main.py
+++ courtney/main.py
@@ -6,7 +6,7 @@ MATO = "https://wowf.com.br/wp-content/uploads/2019/03/ilustracao-768x384.png"
 TARTARUGA = "https://toppng.com/public/uploads/preview/tartaruga-ninja-vermelho-11550709278xvmk3b5qhi.png"
 BECO = "https://micosemental.wordpress.com/files/2009/11/beco.png"
 LADRAO = "https://cdn.pixabay.com/photo/2017/01/30/21/48/burglar-2022159_960_720.png"
-class JohnnyLindo()
+class JohnnyLindo ():
     mato=Cena(img=MATO)
     indio=Elemento(img=INDIO)
     beco=Cena(img=BECO)
