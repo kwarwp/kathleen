cd7ec8c105704a07f867e2ba408fce450b634eb7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-12 11:01:46 -0300

    Saved grace/main.py @2019-11-12 14:01

--- grace/main.py
+++ grace/main.py
@@ -5,14 +5,21 @@ PIRANHA="http://www.pngmart.com/files/7/Piranha-PNG-Free-Download.png"
 ESPACO="https://img2.gratispng.com/20180420/yie/kisspng-light-andromeda-galaxy-outer-space-sky-cosmos-5ad9df10333e65.6204317315242278562099.jpg" 
 INES="http://3.bp.blogspot.com/-rgWJgYjJV7I/UPtVOtHWsfI/AAAAAAAAAB8/StaiQ21jcTA/s1600/2012-12-13-ines-brasil-bbb-13-babado-e-confusao-querida-6.png"
 IGREJA="https://i.pinimg.com/originals/d6/48/be/d648be937077a693a2fcb0ef50a942ef.png"
+CORINGA="https://www.epgrupo.com.br/wp-content/uploads/coringa-216x300-1-216x300.png"
+CASADABARBIE="https://www.mundodomarketing.com.br/mundodomarketing//images/materias/casa-da-barbie.jpg"
 class bruninha ():
     espaco=Cena(img=ESPACO)
     igreja=Cena(img=IGREJA)
+    casadabarbie=Cena(img=CASADABARBIE)
     espaco.direita=igreja
     igreja.esquerda=espaco
+    igreja.direita=casadabarbie
+    casadabarbie.esquerda=igreja
     piranha=Elemento(img=PIRANHA)
     ines=Elemento(img=INES)
+    coringa=Elemento(img=CORINGA)
     piranha.entra(espaco)
     ines.entra(igreja)
+    coringa.entra(casadabarbie)
     espaco.vai()
 bruninha()
