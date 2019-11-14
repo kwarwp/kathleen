da415a75b90eead7c42a57999722a4006bb2da24
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-14 11:56:01 -0300

    Saved parisa/__log__.py @2019-11-14 14:56

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -1260,3 +1260,25 @@ NameError: name 'entra' is not defined
     logo.entra(entra.norte)
 NameError: name 'entra' is not defined
 '''},
+{'date': 'Thu Nov 14 2019 11:56:00.609 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 57
+    OI()
+  module <module> line 23
+    self.todos = todos = Sala( n= SALA1, s= SALA3, l= SALA2, o= SALA4, n1= SALA5, s1= SALA6)
+  module _spy.vitollino.main line 895
+    Sala.c(**kwargs)
+  module _spy.vitollino.main line 923
+    setattr(Sala, nome, Sala(nome=nome, **cena))
+TypeError: string indices must be integers
+'''},
