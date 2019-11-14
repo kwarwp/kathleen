e87cad81bb13790ff3ad1a5d2721dd8db0f92d69
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-14 11:57:06 -0300

    Saved rachel/main.py @2019-11-14 14:57

--- rachel/main.py
+++ rachel/main.py
@@ -6,15 +6,19 @@ MINIOS="https://imagensemoldes.com.br/wp-content/uploads/2018/05/Meu-Malvado-Fav
 MORTI="https://i.pinimg.com/originals/57/79/e9/5779e9e8c85f594eae72c8f5b6ba0d23.png"
 HUCK="http://www.pngall.com/wp-content/uploads/2016/03/Avengers-Hulk-PNG.png"
 MATO="https://pngimage.net/wp-content/uploads/2018/06/mato-png-2.png"
-BARATO="https://pngimage.net/wp-content/uploads/2018/05/barata-voadora-png-1-280x200.png"
+BARATA="https://pngimage.net/wp-content/uploads/2018/05/barata-voadora-png-1-280x200.png"
 class peixe ():
     marte=Cena(img=MARTE)
     morti=Cena(img=MORTI)
+    mato=Cena(img=MATO)
+    barata=Elemento(img=BARATA)
+    barata.entra(mato)
     minios=Elemento(img=MINIOS)
     minios.entra(marte)
     huck=Elemento(img=HUCK)
     huck.entra(morti)
     marte.direita=morti
     morti.esquerda=marte
+    morti.direita=mato
     marte.vai()
 peixe()
