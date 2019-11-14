ce6d7b2044869c9e8e40e10b0b31b076f94fa1ca
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-14 12:02:42 -0300

    Saved parisa/main.py @2019-11-14 15:02

--- parisa/main.py
+++ parisa/main.py
@@ -20,8 +20,10 @@ LOGO= "http://supygirls.pythonanywhere.com/image/camisasuperpython.png"
 NEIDE="https://i.pinimg.com/originals/25/48/27/254827fb621ed6c9e50b401d92554810.png"
 class OI():
     def __init__(self):
-        self.todos = todos = Sala( n= SALA1, s= SALA3, l= SALA2, o= SALA4, n1= SALA5, s1= SALA6)
-        todos.norte.vai()
+        self.todos = todos = Sala( n= SALA1, s= SALA2, l= SALA3, o= SALA4)
+        todos.norte.vai()                         
+        self.todos=todos=Sala(o= SALA4, s= SALA5, l= SALA6, n= SALA1)
+        
         #self.todos = todos = Sala( o= SALA4, s=SALA5, l=SALA6, n=SALA1)
         #todos.norte.vai()
         logo = Elemento(img=LOGO, style=dict(left=370, top=200, width=450, height="250px"))
