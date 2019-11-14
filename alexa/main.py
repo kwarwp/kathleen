be51843b5bfca820d852270c5ee8cd6c5e3024ef
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-14 10:41:25 -0300

    Saved alexa/main.py @2019-11-14 13:41

--- alexa/main.py
+++ alexa/main.py
@@ -3,3 +3,15 @@
 from _spy.vitollino.main import Cena,Elemento,Texto
 from _spy.vitollino.main import inv as Inventario
 
+#fase1 - o cientista entra na casa 
+ENTRADA = "https://i.imgur.com/7eWyvKP.jpg"
+ZEZENHO = "https://i.imgur.com/WQHgtgq.png"
+ROSALINDA = "https://i.imgur.com/BL5Iemf.png"
+SALA1 = ""
+QUADRO1 = ""
+QUADRO2 = ""
+QUADRO3 = ""
+QUADRO4 = ""
+QUADRO5 = ""
+
+
