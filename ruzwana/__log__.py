b9eee83d078c8d1a691250673633d2d4b3c5c498
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-14 12:25:22 -0300

    Saved ruzwana/__log__.py @2019-11-14 15:25

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -702,3 +702,41 @@ TypeError: __init__() got an unexpected keyword argument 'N'
     setattr(Sala, nome, Sala(nome=nome, **cena))
 TypeError: 'Cena' object is not iterable
 '''},
+{'date': 'Thu Nov 14 2019 12:25:21.104 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 24
+    class leni():
+  module <module> line 43
+    haha=Sala(N=casa,S=biblioteca,O=laboratorio,L=porta)
+  module _spy.vitollino.main line 895
+    Sala.c(**kwargs)
+  module _spy.vitollino.main line 923
+    setattr(Sala, nome, Sala(nome=nome, **cena))
+TypeError: 'Cena' object is not iterable
+'''},
