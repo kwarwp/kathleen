de110895348fb49fc3ab0582b8c283147276a52c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-14 11:24:54 -0300

    Saved rachel/main.py @2019-11-14 14:24

--- rachel/main.py
+++ rachel/main.py
@@ -5,6 +5,8 @@ MARTE="https://abrilsuperinteressante.files.wordpress.com/2019/05/site_marte.png
 MINIOS="https://imagensemoldes.com.br/wp-content/uploads/2018/05/Meu-Malvado-Favorito-Minions-Jerry-4-PNG.png"
 MORTI="https://i.pinimg.com/originals/57/79/e9/5779e9e8c85f594eae72c8f5b6ba0d23.png"
 HUCK="http://www.pngall.com/wp-content/uploads/2016/03/Avengers-Hulk-PNG.png"
+MATO="https://pngimage.net/wp-content/uploads/2018/06/mato-png-2.png"
+BARATO="https://pngimage.net/wp-content/uploads/2018/05/barata-voadora-png-1-280x200.png"
 class peixe ():
     marte=Cena(img=MARTE)
     morti=Cena(img=MORTI)
