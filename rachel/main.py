d087cc68a95b31e7ad82e138ff5652c88549e6c1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-14 11:02:08 -0300

    Saved rachel/main.py @2019-11-14 14:02

--- rachel/main.py
+++ rachel/main.py
@@ -3,7 +3,7 @@ from _spy.vitollino.main import Elemento, Cena, Texto
 from _spy.vitollino.main import Inventario as invi 
 MARTE="https://abrilsuperinteressante.files.wordpress.com/2019/05/site_marte.png"
 MINIOS="https://imagensemoldes.com.br/wp-content/uploads/2018/05/Meu-Malvado-Favorito-Minions-Jerry-4-PNG.png"
-MORTI="https://png.pngtree.com/png-clipart/20190611/original/pngtree-vector-fog-png-image_2120607.jpg"
+MORTI="https://i.pinimg.com/originals/57/79/e9/5779e9e8c85f594eae72c8f5b6ba0d23.png"
 HUCK="http://www.pngall.com/wp-content/uploads/2016/03/Avengers-Hulk-PNG.png"
 class peixe ():
     marte=Cena(img=MARTE)
