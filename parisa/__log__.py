b94031ab14ebb983d532b0cc5878f13891ad6aad
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-14 12:11:17 -0300

    Saved parisa/__log__.py @2019-11-14 15:11

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -1350,3 +1350,21 @@ AttributeError: 'str' object has no attribute 'vai'
     sala1.vai()
 NameError: name 'sala1' is not defined
 '''},
+{'date': 'Thu Nov 14 2019 12:11:17.169 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 60
+    OI()
+  module <module> line 26
+    self.todas.todas.vai()
+AttributeError: 'Sala' object has no attribute 'todas'
+'''},
