07182c0e3e307a4ca04ba9be7e84d34e0d02aadc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-14 12:17:44 -0300

    Saved parisa/__log__.py @2019-11-14 15:17

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -1440,3 +1440,21 @@ AttributeError: 'Sala' object has no attribute 'todas'
     self.logo.vai=falalogo.vai
 NameError: name 'falalogo' is not defined
 '''},
+{'date': 'Thu Nov 14 2019 12:17:43.879 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 61
+    OI()
+  module <module> line 39
+    self. neide.vai=falaneide.vai
+NameError: name 'falaneide' is not defined
+'''},
