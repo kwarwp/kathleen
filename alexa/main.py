c36737a7ce38e8fde685dd526a33b8d62c6d81a3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-14 12:56:11 -0300

    Saved alexa/main.py @2019-11-14 15:56

--- alexa/main.py
+++ alexa/main.py
@@ -7,11 +7,12 @@ from _spy.vitollino.main import inv as Inventario
 ENTRADA = "https://i.imgur.com/7eWyvKP.jpg"
 ZEZENHO = "https://i.imgur.com/WQHgtgq.png"
 ROSALINDA = "https://i.imgur.com/BL5Iemf.png"
-SALA1 = ""
-QUADRO1 = ""
-QUADRO2 = ""
-QUADRO3 = ""
-QUADRO4 = ""
-QUADRO5 = ""
+SALA1 = "https://i.imgur.com/MwVOMe6.jpg"
+TELEFONE = "https://i.imgur.com/AWK5NzD.gif"
+QUADRO1 = "https://i.imgur.com/9w6CDLX.png"
+QUADRO2 = "https://i.imgur.com/z4pQgRU.png"
+QUADRO3 = "https://i.imgur.com/z0WhOdq.png"
+COFRE = ""
+
 
 
