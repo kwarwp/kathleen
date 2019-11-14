3e0808b02fcc3bc86ce3426400fc30992eea7c39
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-14 11:31:57 -0300

    Saved rachel/__log__.py @2019-11-14 14:31

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -416,3 +416,29 @@ NameError: name 'Barata' is not defined
     barata=Elemento(img=BARATA)
 NameError: name 'BARATA' is not defined
 '''},
+{'date': 'Thu Nov 14 2019 12:31:55.651 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 10
+    class peixe ():
+  module <module> line 14
+    barata=Elemento(img=BARATA)
+NameError: name 'BARATA' is not defined
+'''},
