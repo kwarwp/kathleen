48bc00e489f4b1aa43c5d6728923254af6f8c4f9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-19 11:31:00 -0300

    Saved courtney/main.py @2019-11-19 14:31

--- courtney/main.py
+++ courtney/main.py
@@ -4,3 +4,14 @@ from _spy.vitollino.main import INVENTARIO as invi
 CIENTISTA = "https://img2.gratispng.com/20180515/ree/kisspng-mad-scientist-famous-scientists-clip-art-5afaa2c000e336.8963581315263751040036.jpg"
 SANGUE = "https://dbdzm869oupei.cloudfront.net/img/sticker/preview/14126.png"
 BIBLIOTECA = "https://daequfpr.files.wordpress.com/2012/04/biblioteca3.jpg"
+CENTROI = "https://www.itforum365.com.br/wp-content/uploads/2017/08/prescriptive-soc.png"
+SETA = "https://image.flaticon.com/icons/png/512/2026/2026737.png"
+PORTA = "https://cdn.pixabay.com/photo/2014/12/21/23/45/door-575962_960_720.png"
+FRAGMENTO = "https://png.pngtree.com/png-clipart/20190117/ourlarge/pngtree-c4d-purple-decoration-triangle-block-png-image_426001.jpg"
+DNA = "https://mpng.pngfly.com/20190508/qs/kisspng-dna-scalable-vector-graphics-computer-icons-gene-kaznmu-kz-pictures-to-pin-on-pinterest-5cd2d75921b124.839995971557321561138.jpg"
+PROTEINA = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Mouse_cholera_antibody.png/170px-Mouse_cholera_antibody.png"
+MOLECULA = "https://images.vexels.com/media/users/3/159421/isolated/preview/e2739354640fee942f38f6283db269e6-c--lula-de-modelo-de-mol--cula-plana-by-vexels.png"
+GENE = "http://wiki.bacterialtakeover.com/images/2/20/Gene_strand.png"
+ATOMOS = "https://img2.gratispng.com/20181120/he/kisspng-vector-graphics-atomic-nucleus-atomsymbol-atom-icons-925-free-vector-icons-5bf4afc71916b8.7770408515427624391028.jpg"
+class yato ():
+   
