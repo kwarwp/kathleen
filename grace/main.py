1388d19b744ba54fdcd04671a0ded33ef2f1b526
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-19 11:22:24 -0300

    Saved grace/main.py @2019-11-19 14:22

--- grace/main.py
+++ grace/main.py
@@ -11,24 +11,42 @@ LABOTY=""
 AJUDANTE=""
 POLICIA=""
 LIVRO=""
+FOLHA=""
+PRACA=""
 class bruninha ():
     laboratorio=Cena(img=LABORATORIO)
     livro=Cena(img=LIVRO)
     laboty=Cena(img=LABOTY)
     charles=Elemento(img=CHARLES)
+    folha=Elemento(img=FOLHA)
+    praca=Cena(img=PRACA)
     laboratorio.direita=laboty
     laboty.direita=livro
+    livro.direita=praca
+    praca.esquerda=livro
     livro.esquerda=laboty
     laboty.esquerda=laboratorio
     charles=Elemento(img=CHARLES)
     ajudante=Elemento(img=AJUDANTE)
     policia=Elemento(img=POLICIA)
+    folha=Elemento(img=FOLHA)
     charles.entra(laboratorio)
     
-    falaines=Texto(igreja, "graças a Deus")
-    ines.vai=falaines.vai
-    coringa=Elemento(img=CORINGA)
-    piranha.entra(espaco)
-    coringa.entra(casadabarbie)
-    espaco.vai()
+    charles=Texto(labotorio, "O que aconteceu aqui? Onde está o meu antídoto?")
+    charles.vai=falacharles.vai
+    
+    ajudante.entra(laboty)
+    
+    ajudante=Texto(laboty, "invadiram a casa do Neytan e o levaram.Por favor chame ajuda!")
+    ajudante.vai=falaajudante.vai
+    
+    charles.entra(livro)
+    policia.entra(laboratorio)
+    
+    policia=Texto(laboratorio, "NÂO MEXE EM NADA! RALA MERMÂO!")
+    
+    
+    
+    
+    
 bruninha()
