9f42bad31320799cf6a7a67e88a1da27b759c5ce
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-19 14:10:16 -0300

    Saved parisa/__log__.py @2019-11-19 17:10

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -1614,3 +1614,10 @@ SyntaxError: invalid syntax
                   ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Nov 19 2019 14:10:13.773 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 16
+  SALA6 = astroce"https://s3-sa-east-1.amazonaws.com/uploads-ntro/blog/wp-content/uploads/2017/04/06121758/sonhar-com-mar.jpg"
+                  ^
+SyntaxError: invalid syntax
+'''},
