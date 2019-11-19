e405d0eae5f37f1a87a9a1a8a3ed32563330406d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-19 15:20:05 -0300

    Saved natalia/main.py @2019-11-19 18:20

--- natalia/main.py
+++ natalia/main.py
@@ -1,8 +1,8 @@
 # kathleen.natalia.main.
 from _spy.vitollino.main import Cena, Elemento
 from _spy.vitollino.main import Inventario as inv
-SAMARA = "s2.glbimg.com/rgj4yr8GEZZHJucynVco0V-gqWk=/s.glbimg.com/jo/eg/f/original/2016/10/31/samara620.jpg"
-ESCADAS = "https://66.media.tumblr.com/af24a5a3d1db25059440d0353d12a9ad/tumblr_mnhotlEilZ1sra95eo1_400.jpg"
+SAMARA = "https://images.fineartamerica.com/images/artworkimages/medium/1/superman-splash-super-hero-series-movie-poster-prints-transparent.png"
+ESCADAS = "https://hypescience.com/wp-content/uploads/2016/05/espaco-tres-dimensoes-3d.jpg"
 class terror():
     samara=Elemento(img=SAMARA)
     escadas=Cena(img=ESCADAS)
