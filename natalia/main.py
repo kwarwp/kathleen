439be2defc320053d1cce3fc4906fc93d40cb103
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-19 15:14:05 -0300

    Saved natalia/main.py @2019-11-19 18:14

--- natalia/main.py
+++ natalia/main.py
@@ -1,17 +1,15 @@
 # kathleen.natalia.main.
 from _spy.vitollino.main import Cena, Elemento
 from _spy.vitollino.main import Inventario as inv
-ESCOLA = "https://images.educamaisbrasil.com.br/content/basico/instituicao/logo/g/colegio-ndi.png"
-SALA = "http://www.adel.org.br/wp-content/uploads/2016/07/Sala-de-Aula.png"
-ALUNO = "http://www.ifmg.edu.br/ceadop3/imagens/aluno.png/@@images/image.png"
-PROFESSOR = "https://upload.wikimedia.org/wikipedia/commons/b/b1/Professor.png"
-class pibic():
-    escola=Cena(img=ESCOLA)
-    sala=Cena(img=SALA)
-    professor=Elemento(img=PROFESSOR)
-    aluno=Elemento(img=ALUNO)
-    professor.entra(sala)
-    aluno.entra(escola)
-    sala.vai
-    escola.vai
-pibic()
+SAMARA = "s2.glbimg.com/rgj4yr8GEZZHJucynVco0V-gqWk=/s.glbimg.com/jo/eg/f/original/2016/10/31/samara620.jpg"
+ESCADAS = "https://66.media.tumblr.com/af24a5a3d1db25059440d0353d12a9ad/tumblr_mnhotlEilZ1sra95eo1_400.jpg"
+class terror():
+    samara=Elemento(img=SAMARA)
+    escadas=Cena(img=Escadas)
+    samara.entra(escadas)
+    escadas.vai
+terror()
+    
+    
+    
+
