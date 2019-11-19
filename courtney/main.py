7a3c709e0a938b41ae19104b023e6e4f325b0daa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-19 11:17:29 -0300

    Saved courtney/main.py @2019-11-19 14:17

--- courtney/main.py
+++ courtney/main.py
@@ -1,21 +1,6 @@
 # kathleen.courtney.main.py
 from _spy.vitollino.main import Cena, Elemento, Texto
 from _spy.vitollino.main import INVENTARIO as invi
-INDIO = "https://cdn.pixabay.com/photo/2019/03/14/21/55/apache-4056009_960_720.png"
-MATO = "https://wowf.com.br/wp-content/uploads/2019/03/ilustracao-768x384.png"
-TARTARUGA = "https://toppng.com/public/uploads/preview/tartaruga-ninja-vermelho-11550709278xvmk3b5qhi.png"
-BECO = "https://micosemental.wordpress.com/files/2009/11/beco.png"
-LADRAO = "https://cdn.pixabay.com/photo/2017/01/30/21/48/burglar-2022159_960_720.png"
-class JohnnyLindo ():
-    mato=Cena(img=MATO)
-    indio=Elemento(img=INDIO)
-    indio.entra(mato)
-    beco=Cena(img=BECO)
-    tartaruga=Elemento(img=TARTARUGA)
-    ladrao=Elemento(img=LADRAO)
-    ladrao.entra(beco)
-    tartaruga.entra(beco)
-    mato.direita=beco
-    beco.esquerda=mato
-    mato.vai()
-JohnnyLindo()
+CIENTISTA = "https://img2.gratispng.com/20180515/ree/kisspng-mad-scientist-famous-scientists-clip-art-5afaa2c000e336.8963581315263751040036.jpg"
+SANGUE = "https://dbdzm869oupei.cloudfront.net/img/sticker/preview/14126.png"
+BIBLIOTECA = "https://daequfpr.files.wordpress.com/2012/04/biblioteca3.jpg"
