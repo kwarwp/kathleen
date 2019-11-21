e1f60687cad47affb6dc412fb75de3b6844e533b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-21 14:28:06 -0300

    Saved parisa/__log__.py @2019-11-21 17:28

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -1642,3 +1642,21 @@ SyntaxError: keyword argument repeated
                                                                                                                          ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Thu Nov 21 2019 14:28:09.834 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 64
+    OI()
+  module <module> line 26
+    self.todas=todas= Sala(o= SALA5, s= SALA6, l= SALA7, n= SALA1)
+NameError: name 'SALA7' is not defined
+'''},
