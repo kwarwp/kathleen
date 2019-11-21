df98f155d9bf7751f48f05de642764ed6876d40d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-21 13:52:28 -0300

    Saved parisa/main.py @2019-11-21 16:52

--- parisa/main.py
+++ parisa/main.py
@@ -37,7 +37,7 @@ class OI():
         
         self.neide=Elemento(img=NEIDE, style=dict(left=100, top=350, width=300, height="200px"))
         self. neide.entra(todos.leste)
-        self.falaneide= Texto(todos.leste, "Seja bem vindo! Esse é o Colégio _______ e estamos contentes em recebe-lo, espero que se adapte a sua nossa vida acadêmica.")
+        self.falaneide= Texto(todos.leste, "Seja bem vindo! Esse é o Colégio _______ e estamos contentes em recebe-lo, espero que se adapte a sua nova vida acadêmica.")
         self. neide.vai=self.falaneide.vai
         
         self.menino = Elemento(img=MENINO, style=dict(left=600, top=350, width=300, height="200px"))
