ef8ae8ba50fa9c331bf8fc7f769b454113649356
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-26 10:45:14 -0300

    Saved kristen/main.py @2019-11-26 13:45

--- kristen/main.py
+++ kristen/main.py
@@ -44,8 +44,7 @@ class CARTA():
     estante=Cena (img=ESTANTE)
     casa=Cena (img=CASA)
     
-    piu=Labirinto(N=casa, S=corredor, L=cofre ,O=estante)
-    piu.vai()
+    casa.direita=corredor
     
 CARTA()
     
