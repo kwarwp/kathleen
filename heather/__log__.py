ca156c0d44efa16b79295ccc5b6733e724bfb2bc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-26 11:38:51 -0300

    Saved heather/__log__.py @2019-11-26 14:38

--- heather/__log__.py
+++ heather/__log__.py
@@ -20,3 +20,10 @@ SyntaxError: invalid syntax
                     ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Nov 26 2019 11:38:50.251 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 13
+  laboratorio=Cena (img=LABORATORIO, tit="laboratorio", top=50, left)
+                                                                    ^
+SyntaxError: non-keyword arg after keyword arg
+'''},
