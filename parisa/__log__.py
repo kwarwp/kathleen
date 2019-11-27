d15571f99dc7c860d0b23a79d9c23eb5dcefddf4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 18:26:39 -0300

    Saved parisa/__log__.py @2019-11-27 21:26

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -1778,3 +1778,10 @@ NameError: name 'canada' is not defined
                                                           ^
 SyntaxError: non-keyword arg after keyword arg
 '''},
+{'date': 'Wed Nov 27 2019 18:26:38.568 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 103
+  self.menino = Elemento(img=MENINO, style=dict(left=600,top,350,width300,height="200px"))
+                                                            ^
+SyntaxError: non-keyword arg after keyword arg
+'''},
