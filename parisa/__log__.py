236fee7f6f95d3624d861f4c3d3da3e74eed1625
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 16:06:51 -0300

    Saved parisa/__log__.py @2019-11-27 19:06

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -1681,3 +1681,21 @@ IndentationError: unexpected indent
            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Nov 27 2019 16:06:50.724 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 76
+    OI()
+  module <module> line 58
+    self.a=Elemento(img=A, style=dict(left=600, top=350, width=300, height="200px"))
+NameError: name 'A' is not defined
+'''},
