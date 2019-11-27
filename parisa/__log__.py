ff017ebf64a3deb5b45003c3a24a1c1f0b9b916d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 18:37:13 -0300

    Saved parisa/__log__.py @2019-11-27 21:37

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -1806,3 +1806,25 @@ IndentationError: unexpected indent
                            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Nov 27 2019 18:37:12.562 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 144
+    OI()
+  module <module> line 44
+    e=elm(todas)
+  module <module> line 75
+    self.a.vai()
+  module <module> line 78
+    self.todas.leste.vai()
+AttributeError: 'elm' object has no attribute 'todas'
+'''},
