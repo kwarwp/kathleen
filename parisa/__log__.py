0b3371eebe98e2899938a1f9264cbf2a753b111d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 19:05:24 -0300

    Saved parisa/__log__.py @2019-11-27 22:05

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -1981,3 +1981,23 @@ SyntaxError: invalid syntax
     self.a=Elemento(img=A, style=dict(left=100, top=100, width=50, height="200px"),vai=self.acertou)
 AttributeError: 'hey' object has no attribute 'acertou'
 '''},
+{'date': 'Wed Nov 27 2019 19:05:23.122 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 143
+    OI()
+  module <module> line 42
+    d=hey(todas)
+  module <module> line 68
+    self.a=Elemento(img=A, style=dict(left=100, top=100, width=50, height="200px"),vai=self.acertou)
+AttributeError: 'hey' object has no attribute 'acertou'
+'''},
