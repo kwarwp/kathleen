4f55d4d17b0f310752b239be524a6a9d88bd3d79
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 18:40:00 -0300

    Saved parisa/__log__.py @2019-11-27 21:40

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -1828,3 +1828,21 @@ SyntaxError: invalid syntax
     self.todas.leste.vai()
 AttributeError: 'elm' object has no attribute 'todas'
 '''},
+{'date': 'Wed Nov 27 2019 18:39:59.176 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 145
+    OI()
+  module <module> line 45
+    todas.sul.vai=e.vai
+AttributeError: 'elm' object has no attribute 'vai'
+'''},
