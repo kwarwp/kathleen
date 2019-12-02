76c5e2ff184104d6b1177054ed6b5a0a85dc168f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-02 12:22:24 -0300

    Saved parisa/__log__.py @2019-12-02 15:22

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -2019,3 +2019,23 @@ AttributeError: 'hey' object has no attribute 'acertou'
     d=hey(todas)
 UnboundLocalError: local variable 'todas' referenced before assignment
 '''},
+{'date': 'Mon Dec 02 2019 12:22:20.946 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 149
+    OI()
+  module <module> line 41
+    c=ajuda(todos)
+  module <module> line 127
+    self.logo = Elemento(img=LOGO, style=dict(left=370, top=200, width=450, height="250px"))
+NameError: name 'LOGO' is not defined
+'''},
