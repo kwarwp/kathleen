8741998f652c481a2e9c6c4e754319f5aeceb644
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-02 20:50:31 -0300

    Created kathryn/__log__.py @2019-12-02 23:50

---
+++ kathryn/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Dec 02 2019 20:50:28.942 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 39
+  proteina.entra(centro
+                             ^
+SyntaxError: invalid syntax
+'''},
