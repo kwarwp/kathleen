4aa30fa7c1106664b4ba275df4e41803e328857e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 08:51:31 -0300

    Saved parisa/__log__.py @2019-12-04 11:51

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -2212,3 +2212,23 @@ SyntaxError: invalid syntax
                                       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Dec 04 2019 08:51:25.472 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 178
+    OI()
+  module <module> line 40
+    a=cansada(todos)
+  module <module> line 129
+    self.todas.sul.vai
+AttributeError: 'cansada' object has no attribute 'todas'
+'''},
