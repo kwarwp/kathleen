ab8ae38646aa9e638c9f0b24d2c42bf65dd09824
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 09:03:01 -0300

    Saved parisa/main.py @2019-12-04 12:03

--- parisa/main.py
+++ parisa/main.py
@@ -141,7 +141,7 @@ class  blabla():
     
      def vai(self,ev=0):
         self.foi()
-        Texto(self.todos.sul,"Estamos aqui na sala de aula e voce tera que fazer sua escolha: Qual deles sofre bullying ?").vai()
+        Texto(self.todos.sul,"Estamos aqui na sala de aula e você tera que fazer sua escolha: Qual deles sofre bullying ? Cada personagem está contando sua história. Passe o mouse em cima de cada um e conheça mais sobre o dia a dia deles.").vai()
         
         
 class ajuda():
