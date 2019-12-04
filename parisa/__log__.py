b04f1a4eeea204181b169852e69a9144a8a06b8d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 08:25:31 -0300

    Saved parisa/__log__.py @2019-12-04 11:25

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -2171,3 +2171,23 @@ SyntaxError: invalid syntax
                                                                                                                                                                      ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Wed Dec 04 2019 08:25:26.192 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 168
+    OI()
+  module <module> line 36
+    d=hey(todas)
+  module <module> line 92
+    self.c.entra(todas.sul)
+AttributeError: 'hey' object has no attribute 'c'
+'''},
