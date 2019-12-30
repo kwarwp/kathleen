d1209369049a9416f66e73d4312b08f71f9f34db
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-30 11:37:44 -0300

    Saved ruzwana/__log__.py @2019-12-30 14:37

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -1320,3 +1320,29 @@ TypeError: __init__() got an unexpected keyword argument 'N'
     haha=Labirinto(N=casa,S=biblioteca,O=laboratorio,L=porta)
 TypeError: __init__() got an unexpected keyword argument 'N'
 '''},
+{'date': 'Mon Dec 30 2019 11:38:10.994 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 24
+    class leni():
+  module <module> line 43
+    haha=Labirinto(N=casa,S=biblioteca,O=laboratorio,L=porta)
+TypeError: __init__() got an unexpected keyword argument 'N'
+'''},
