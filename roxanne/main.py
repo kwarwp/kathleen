6a8d1f4bd3ab18e2502edd71500f67d6513bbc57
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-01-06 19:14:38 -0300

    Saved roxanne/main.py @2020-01-06 22:14

--- roxanne/main.py
+++ roxanne/main.py
@@ -25,7 +25,7 @@ class doidera():
         #laboratorio= Cena(img=LABORATORIO)
         pass
 class Personagens():
-    def __init__(self,imagem = ANA_MARIA, nome ="Aninha"):
+    def __init__(self,imagem = ANA_MARIA, nome ="Ola, eu sou a Ana Paula é um prazer estar com vocês, sou mestra e professorA DE BIOLOGIA e estou aqui para ajudar vocês nos conceitos de genéticas e nas provas que terão, aprendendo de uma maneira divertida"):
         self.ana_maria= Elemento(img=imagem, tit=nome, drag=True, style=dict(
             left=150,top=330,width=300,heigth="250px"))
         #dr_zukman=Elemento(img=DR_ZUKMAN, style=dict(
