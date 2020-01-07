9a2455566b9d0f3f4ac03737a3601217af0b7410
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-01-07 16:29:39 -0300

    Saved roxanne/__log__.py @2020-01-07 19:29

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -471,3 +471,21 @@ AttributeError: 'Personagens' object has no attribute 'vai'
     Texto(self.congresso.norte, "Bem vindos à FioCruz").vai()
 AttributeError: 'Cenas' object has no attribute 'congresso'
 '''},
+{'date': 'Tue Jan 07 2020 16:29:38.308 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 183
+    TakeFioCruz()
+  module <module> line 39
+    congresso.norte.vai=d.vai
+AttributeError: 'Personagens' object has no attribute 'vai'
+'''},
