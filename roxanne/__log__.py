397c28d398efa6b7dbcb4b9c71226843ac1ccfcb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-01-07 16:01:05 -0300

    Saved roxanne/__log__.py @2020-01-07 19:01

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -355,3 +355,23 @@ UnboundLocalError: local variable 'congresso' referenced before assignment
     PERSONAGEM.entra(congresso.norte)
 AttributeError: 'NoneType' object has no attribute 'entra'
 '''},
+{'date': 'Tue Jan 07 2020 16:01:03.996 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 173
+    TakeFioCruz()
+  module <module> line 35
+    a=Cenas(congresso, d)
+  module <module> line 71
+    self,personagem = personagem
+TypeError: 'Personagens' object is not iterable
+'''},
