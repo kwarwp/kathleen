b9c7d13169301a1165963985f605a4da4e8a61cd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-01-07 15:53:03 -0300

    Saved roxanne/__log__.py @2020-01-07 18:53

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -277,3 +277,23 @@ SyntaxError: invalid syntax
     Cenas()
 TypeError: __init__() missing 1 positional argument: congresso
 '''},
+{'date': 'Tue Jan 07 2020 15:53:00.671 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 172
+    TakeFioCruz()
+  module <module> line 34
+    c=Mendel(congresso)
+  module <module> line 84
+    self.foi,congresso.oeste.vai=congresso.SALA3.vai,self.vai
+AttributeError: 'Sala' object has no attribute 'SALA3'
+'''},
