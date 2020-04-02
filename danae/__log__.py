682203d12b99112b9770c84cf6c218f1f0b34f07
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-04-01 23:58:31 -0300

    Saved danae/__log__.py @2020-04-02 02:58

--- danae/__log__.py
+++ danae/__log__.py
@@ -265,3 +265,21 @@ NameError: name 'JOGADOR' is not defined
     jogador= Elemento(img=JOGADOR)
 NameError: name 'JOGADOR' is not defined
 '''},
+{'date': 'Wed Apr 01 2020 23:58:30.594 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 11
+    class leni():
+  module <module> line 14
+    caixinha= Elemento(img=CAIXADEMUSICA)
+NameError: name 'CAIXADEMUSICA' is not defined
+'''},
