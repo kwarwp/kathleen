33aa2ddc167bca9370ae920fd54bd3f7bdf4f701
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-07-20 23:28:32 -0300

    Saved stacy/__log__.py @2020-07-21 02:28

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -159,3 +159,10 @@ SyntaxError: invalid syntax
                                                     ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Jul 20 2020 23:31:44.407 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 21
+  estagiario = Elemento(img=ESTAGIARIO,tit="Elton" style=dict(left=100, top=10, width=60, height="60px"))
+                                                    ^
+SyntaxError: invalid syntax
+'''},
