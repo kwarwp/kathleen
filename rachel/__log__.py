faac244967898f9d89deb6d33f5f395eaa189c4e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-11-18 19:17:35 -0300

    Saved rachel/__log__.py @2020-11-18 22:17

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -631,3 +631,10 @@ SyntaxError: invalid syntax
          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Nov 18 2020 19:17:35.93 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 10
+  BRUXA=
+         ^
+SyntaxError: invalid syntax
+'''},
