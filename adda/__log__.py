24ce3bf12777d16a8ca344caebdec3d9d4e89c45
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-09-20 15:15:15 -0300

    Saved adda/__log__.py @2023-09-20 18:15

--- adda/__log__.py
+++ adda/__log__.py
@@ -103,3 +103,10 @@ SyntaxError: invalid syntax
                                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Sep 20 2023 15:14:55.420 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 16
+  self.oswaldin = Elemento(img=)
+                                ^
+SyntaxError: invalid syntax
+'''},
