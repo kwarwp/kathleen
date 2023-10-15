59d0696d649da0c30da41ad1efeaef335407442b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-15 11:59:59 -0300

    Saved sarah/__log__.py @2023-10-15 14:59

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -635,3 +635,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Sun Oct 15 2023 11:59:59.87 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  self.npc=Elemento(img="https://i.imgur.com/GbnwrWN.png") 
+  ^
+IndentationError: expected an indented block
+'''},
