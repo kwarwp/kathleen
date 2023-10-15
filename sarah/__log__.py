4c7cbe032b8eed909bfdde043d9cfd2a5d1af883
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-15 11:53:04 -0300

    Saved sarah/__log__.py @2023-10-15 14:53

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -504,3 +504,21 @@ KeyError: img
                                                                                                                                                                                                                                                                                          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sun Oct 15 2023 11:53:04.137 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 7
+    class terror():
+  module <module> line 9
+    TEXTO(self.npc, "NPC: Acabamos de receber uma mensagem da NASA informando que um dos satélites no espaço detectou um objeto no interior de um núcleo de gelo na região antártica")
+NameError: name 'TEXTO' is not defined
+'''},
