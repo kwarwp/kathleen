d6c44db02ad9c5b9a57a04385c69fe68ae2830a8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-15 11:47:54 -0300

    Saved sarah/main.py @2023-10-15 14:47

--- sarah/main.py
+++ sarah/main.py
@@ -1,13 +1,11 @@
 # kathleen.sarah.main.py
 from _spy.vitollino.main import Cena, Elemento, Labirinto, Texto, Codigo, Sala, STYLE
 from _spy.vitollino.main import INVENTARIO as inv
-STYLE["width"] = 1150
-STYLE["height"] = "600px"
-INICIO="https://imgur.com/yJuD0vq"
-
-class vai():
-    inicio=Cena(img=INICIO)
-    npc=Elemento(img="https://imgur.com/GbnwrWN", tit="NPC: Acabamos de receber uma mensagem da NASA informando que um dos satélites no espaço detectou um objeto no interior de um núcleo de gelo na região antártica.")
+INICIO = "https://i.imgur.com/vFALZ8K.jpg"
+class terror():
+    npc=Elemento(img="https://i.imgur.com/GbnwrWN.png")
+    inicio=Cena(img=INICIO , tit = "NPC: Acabamos de receber uma mensagem da NASA informando que um dos satélites no espaço detectou um objeto no interior de um núcleo de gelo na região antártica")
     npc.entra(inicio)
     inicio.vai()
-vai()    
+terror()
+    
