3575ba5767e9bae252398dcfdea3cf3d0284b1cc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-16 19:26:10 -0300

    Saved sarah/__log__.py @2023-10-16 22:26

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -649,3 +649,21 @@ IndentationError: expected an indented block
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Oct 16 2023 19:26:09.963 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 16
+    class oi():
+  module <module> line 17
+    Texto(inicio, "Sua missão é encontrá-lo! Mas antes você precisará fazer algumas escolhas.").vai()
+NameError: name 'inicio' is not defined
+'''},
