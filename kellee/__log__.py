0191bd712d1fc760c8fc4ba990b99b35956c2fde
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-01-31 19:23:53 -0300

    Saved kellee/__log__.py @2024-01-31 22:23

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -466,3 +466,10 @@ AttributeError: 'ajuda' object has no attribute 'dr'
                                                           ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Jan 31 2024 19:23:51.396 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 77
+  self.c=Elemento(img= "https://i.imgur.com/MRYFB9o.png" ,tit"HCL",   
+                                                              ^
+SyntaxError: invalid syntax
+'''},
