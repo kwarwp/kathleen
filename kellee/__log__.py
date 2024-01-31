9673a55be1b1a64eb06e3c365d694ac1cf098233
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-01-31 20:29:55 -0300

    Saved kellee/__log__.py @2024-01-31 23:29

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -1103,3 +1103,23 @@ AttributeError: 'ajuda' object has no attribute 'dr'
     self.dr.entra
 AttributeError: 'ajuda' object has no attribute 'dr'
 '''},
+{'date': 'Wed Jan 31 2024 20:29:53.297 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 182
+    OI()
+  module <module> line 41
+    e=pol(todas)
+  module <module> line 105
+    self.foi,todas.oeste.vai=todas.oeste.vai,self.vai
+AttributeError: 'pol' object has no attribute 'vai'
+'''},
