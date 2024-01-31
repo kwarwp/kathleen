f1c65fa073f68fe3fd11cbbe6099ea65e7be0cb5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-01-31 17:56:11 -0300

    Created kellee/__log__.py @2024-01-31 20:56

---
+++ kellee/__log__.py
@@ -0,0 +1,21 @@
+
+{'date': 'Wed Jan 31 2024 17:56:07.688 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 168
+    OI()
+  module <module> line 36
+    a=cansada(todos)
+  module <module> line 106
+    self.neide=Elemento(img=NEIDE, style=dict(left=100, top=350, width=300, height="200px"))
+NameError: name 'NEIDE' is not defined
+'''},
