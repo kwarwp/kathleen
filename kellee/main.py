7620a1822e5b43d2a02821fc08d1adcdea596b0e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-01-31 18:49:36 -0300

    Saved kellee/main.py @2024-01-31 21:49

--- kellee/main.py
+++ kellee/main.py
@@ -144,8 +144,8 @@ class ajuda():
         self.todos=todos
         self.dra= Elemento ( img=NEIDE, tit="Para o alívio dos sintomas você vai precisar tomar um omeprazol ao acordar em jejum.", style=dict(left=100, top=300, width=300, height="200px"))
         self.dra.entra(todos.norte)
-        self.dra= Elemento ( img= "https://imgur.com/a/aDBS6Mf", tit="Por que em jejum, dra?", style=dict(left=570, top=300, width=300, height="200px"))
-        self.dra.entra(todos.norte)
+        self.paci= Elemento ( img= "https://imgur.com/a/aDBS6Mf", tit="Por que em jejum, dra?", style=dict(left=570, top=300, width=300, height="200px"))
+        self.paci.entra(todos.norte)
         
         self.play = Elemento(img=PLAY, tit="Jogar", style=dict(left=370, top=300, width=300, height="200px"))
         self.play.entra(todos.norte)
