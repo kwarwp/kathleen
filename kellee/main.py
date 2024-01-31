50c04e638f60a468bb8623624d65a2521860dfb8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-01-31 19:23:41 -0300

    Saved kellee/main.py @2024-01-31 22:23

--- kellee/main.py
+++ kellee/main.py
@@ -7,16 +7,16 @@ STYLE["height"] = "600px"
 SALA1 = "https://i.imgur.com/5cKwnkv.jpg"
 #fachada da escola CENA 1
 SALA2 = "https://i.imgur.com/5cKwnkv.jpg"
-#Sala de aula CENA 2
+#bancada
 SALA3 = "https://i.imgur.com/tiJmoBA.jpeg"
 #Corredor da escola CENA 4
-SALA4 = "https://i.imgur.com/FBbMoQM.jpg"
+SALA4 = "https://i.imgur.com/tiJmoBA.jpeg"
 #Quadra de esportes
 SALA5 = "https://i.imgur.com/B2CtFTa.jpg"
 #desconhecida 
 SALA6 = "https://s3-sa-east-1.amazonaws.com/uploads-ntro/blog/wp-content/uploads/2017/04/06121758/sonhar-com-mar.jpg"
 #MENINO= "https://cdn.pixabay.com/photo/2017/07/07/03/21/child-2480290_960_720.png"
-PLAY= "https://i.imgur.com/G9QfpN5.png"
+PLAY= "https://i.imgur.com/aVH9hm5.png"
 #diretora neide RASCUNHO
 NEIDE=" https://i.imgur.com/ATlnIzM.png"
 PACIENTE="https://i.imgur.com/mcBVqNT.png"
@@ -74,13 +74,13 @@ class hey():
         self.todas=todas
         #self.menino = Elemento(img=MENINO, style=dict(left=600, top=350, width=300, height="200px"))
         # self.menino.entra(todas.sul)
-        self.c=Elemento(img= "https://i.imgur.com/lQDHDC4.png", tit="Oiiii, meu nome é Toni. Eu tenho 13 anos e estou tão triste. Meus amigos tem me chamado de coisas horriveis e isso me deixa triste. - O trabalho foi baseado nos números da Pesquisa Nacional de Saúde do Escolar (PeNSE) de 2012, que entrevistou 110 mil alunos, representando um universo de mais de três milhões de crianças. O estudo traçou o perfil antropométrico dos estudantes a partir da percepção deles sobre o próprio corpo, numa escala de características como âÂÂmuito magroâÂÂ, âÂÂmagroâÂÂ, âÂÂnormalâÂÂ, âÂÂgordoâÂÂ e âÂÂmuito gordoâÂÂ. Após comparar as respostas das entrevistas, a pesquisa chegou à conclusão que os estereótipos âÂÂmuito magroâÂÂ, âÂÂgordoâÂÂ e âÂÂmuito gordoâÂÂ são os alvos preferidos de perseguições: 11,3%, 12,1% e 23,7% das crianças inseridas nestas categorias, respectivamente, responderam sofrer bullying com frequência- 'https://oglobo.globo.com/sociedade/educacao/alunos-acima-do-peso-sao-mais-vitimas-de-bullying-na-escola-12375170'",   
+        self.c=Elemento(img= "https://i.imgur.com/MRYFB9o.png" tit"HCL",   
         style=dict(left=50, top=350, width=400, height="200px"),vai=self.acertou)
         
-        self.b=Elemento(img=B, tit= "Dani aqui, pessoaaalll! Vocês acreditam que a professora hoje me obrigou a fazer aquele esporte nojento???? Futebol, eca! É coisa de menino. Fora que eu comprei um short lindissmo rosa e adivinham??? Não combinou com esse uniforme." ,
+        self.b=Elemento(img= "https://i.imgur.com/1JI8RPi.png", tit= "Leite" ,
         style=dict (left=400, top=350, width=300, height="200px",),vai=self.errou)
 
-        self.a=Elemento(img= "https://i.imgur.com/KaEaDQk.png", tit = "Fala aêê. Meu nome é Rodrigo e hoje eu estou suavee na nave. Dia de futebooool e de zoar o Toni. AQUELE BALEIA DE ÓCULOS ALI KKKKKKK",
+        self.a=Elemento(img= "https://i.imgur.com/FUyf2pz.png", tit = "Água",
         style=dict(left=700, top=350, width=400, height="200px"),vai=self.errou)
 
         self.a.entra(todas.sul)
@@ -102,6 +102,7 @@ class hey():
 
 class cansada():
     def __init__(self,todos):
+    #lestenaoexiste
         self.foi,todos.leste.vai=todos.leste.vai,self.vai
         self.todos=todos
         #self.neide=Elemento(img=NEIDE, style=dict(left=100, top=350, width=300, height="200px"))
@@ -136,7 +137,7 @@ class  blabla():
     
      def vai(self,ev=0):
         self.foi()
-        Texto(self.todos.sul,"Para provar ao paciente sobre o uso do remédio em jejum podemos fazer um experimento. Eles são uma maneira prática de aprender e testar seus conhecimentos.Quando um medicamento é ingerido, ele vai para o estômago e parte de seu princípio ativo já é absorvida, entrando na corrente sanguínea. Para demonstrar ao paciente que o medicamento Omeprazol é melhor absorvido em jejum, devemos realizar um experimento simulando a composição existente no estômago. Qual das vidrarias você acha que seria ideal?").vai()
+        Texto(self.todos.sul,"Para provar ao paciente sobre o uso do remédio em jejum podemos fazer um experimento. Eles são uma maneira prática de aprender e testar seus conhecimentos.Quando um medicamento é ingerido, ele vai para o estômago e parte de seu princípio ativo já é absorvida, entrando na corrente sanguínea. Para demonstrar ao paciente que o medicamento Omeprazol é melhor absorvido em jejum, devemos realizar um experimento simulando a composição existente no estômago. Qual das vidrarias você acha que seria ideal para iniciar o experimento?").vai()
         
         
 class ajuda():
@@ -145,7 +146,7 @@ class ajuda():
         self.todos=todos
         self.dra= Elemento ( img=NEIDE, tit="Para o alívio dos sintomas você vai precisar tomar um omeprazol ao acordar em jejum.", style=dict(left=100, top=300, width=300, height="200px"))
         self.dra.entra(todos.norte)
-        self.paciente= Elemento ( img= PACIENTE, tit="Por que em jejum, dra?", style=dict(left=870, top=60, width=300, height="500px"))
+        self.paciente= Elemento ( img= PACIENTE, tit="Por que em jejum, dra?", style=dict(left=870, top=120, width=300, height="500px"))
         self.paciente.entra(todos.norte)
         
         self.play = Elemento(img=PLAY, tit="Jogar", style=dict(left=370, top=300, width=300, height="200px"))
